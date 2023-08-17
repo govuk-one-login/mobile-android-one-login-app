561e2819530d6b5ceb0289ebe76f953c55f1d637
Chuddington <6558350+Chuddington@users.noreply.github.com>
2023-08-17 10:26:27 +0100

    Use operator fun notation; pass dependency fun in
    - Use `forEach(::dependencyFunction)` notation for implementation and
      androidTestImplementation dependencies.
    - Use `get` operator function shorthand `object[index]` for root project
      property access.
    
    Resolves: DCMAW-6156

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,13 +4,13 @@ plugins {
 }
 
 android {
-    namespace = rootProject.ext.get("appId") as String
-    compileSdk = rootProject.ext.get("compileSdkVersion") as Int
+    namespace = rootProject.ext["appId"] as String
+    compileSdk = rootProject.ext["compileSdkVersion"] as Int
 
     defaultConfig {
-        applicationId = rootProject.ext.get("appId") as String
-        minSdk = rootProject.ext.get("minSdkVersion") as Int
-        targetSdk = rootProject.ext.get("targetSdkVersion") as Int
+        applicationId = rootProject.ext["appId"] as String
+        minSdk = rootProject.ext["minSdkVersion"] as Int
+        targetSdk = rootProject.ext["targetSdkVersion"] as Int
         versionCode = 1
         versionName = "1.0"
 
@@ -42,9 +42,7 @@ dependencies {
     listOf(
         "androidx.test.ext:junit:1.1.5",
         "androidx.test.espresso:espresso-core:3.5.1",
-    ).forEach { dep: Any ->
-        androidTestImplementation(dep)
-    }
+    ).forEach(::androidTestImplementation)
 
     listOf(
         "androidx.core:core-ktx:1.10.1",
@@ -53,9 +51,7 @@ dependencies {
         "androidx.constraintlayout:constraintlayout:2.1.4",
         "androidx.navigation:navigation-fragment-ktx:2.7.0",
         "androidx.navigation:navigation-ui-ktx:2.7.0",
-    ).forEach { dep: Any ->
-        implementation(dep)
-    }
+    ).forEach(::implementation)
 
     testImplementation("junit:junit:4.13.2")
 }
