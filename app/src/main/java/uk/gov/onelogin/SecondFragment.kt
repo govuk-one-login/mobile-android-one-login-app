31d8dfe8d29cfae3fa60d86af2ac8c3e832e660f
Chuddington <6558350+Chuddington@users.noreply.github.com>
2023-08-17 11:45:50 +0100

    - Lower version of jetpack navigation dependencies. Provide a `because`   clause. - Rename package structure to be prefixed with `uk.gov`.
    Resolves: DCMAW-6156

--- app/src/main/java/uk/gov/onelogin/SecondFragment.kt
+++ app/src/main/java/uk/gov/onelogin/SecondFragment.kt
@@ -1,4 +1,4 @@
-package com.example.onelogin
+package uk.gov.onelogin
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.onelogin.databinding.FragmentSecondBinding
+import uk.gov.onelogin.databinding.FragmentSecondBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
