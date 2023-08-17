04923570a91dbccc2b1211a9dcfaff95dce43afd
Chuddington <6558350+Chuddington@users.noreply.github.com>
2023-08-17 10:06:45 +0100

    Reduce duplication; create root project properties
    - Use `listOf` notation for the dependency collections, minimising the
      use of `implementation` and `androidTestImplementation` blocks.
    - Create ext properties in the root project for the following:
      * App ID
      * Compile Android SDK version
      * Minimum Android SDK version
      * Target Android SDK version
    
    Resolves: DCMAW-6156

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,13 +4,13 @@ plugins {
 }
 
 android {
-    namespace = "com.example.onelogin"
-    compileSdk = 33
+    namespace = rootProject.ext.get("appId") as String
+    compileSdk = rootProject.ext.get("compileSdkVersion") as Int
 
     defaultConfig {
-        applicationId = "com.example.onelogin"
-        minSdk = 29
-        targetSdk = 33
+        applicationId = rootProject.ext.get("appId") as String
+        minSdk = rootProject.ext.get("minSdkVersion") as Int
+        targetSdk = rootProject.ext.get("targetSdkVersion") as Int
         versionCode = 1
         versionName = "1.0"
 
@@ -22,7 +22,7 @@ android {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                "proguard-rules.pro",
             )
         }
     }
@@ -39,14 +39,23 @@ android {
 }
 
 dependencies {
+    listOf(
+        "androidx.test.ext:junit:1.1.5",
+        "androidx.test.espresso:espresso-core:3.5.1",
+    ).forEach { dep: Any ->
+        androidTestImplementation(dep)
+    }
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    listOf(
+        "androidx.core:core-ktx:1.10.1",
+        "androidx.appcompat:appcompat:1.6.1",
+        "com.google.android.material:material:1.9.0",
+        "androidx.constraintlayout:constraintlayout:2.1.4",
+        "androidx.navigation:navigation-fragment-ktx:2.7.0",
+        "androidx.navigation:navigation-ui-ktx:2.7.0",
+    ).forEach { dep: Any ->
+        implementation(dep)
+    }
+
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
