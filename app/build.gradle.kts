e3bee9cda119a5b846754fe202343d8798481df3
Chuddington <6558350+Chuddington@users.noreply.github.com>
2023-08-22 12:11:35 +0100

    Create customisable actions for check / upload
    - Enable test coverage for unit and instrumentation tests for the debug
      build type.
    
    Resolves: DCMAW-6087

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,10 @@ android {
                 "proguard-rules.pro",
             )
         }
+        debug {
+            enableUnitTestCoverage = true
+            enableAndroidTestCoverage = true
+        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
