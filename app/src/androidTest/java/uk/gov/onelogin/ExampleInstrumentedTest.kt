3df75625c2ea88b24901f13a6ab9c5965e56a1bc
Chuddington <6558350+Chuddington@users.noreply.github.com>
2023-08-23 14:18:38 +0100

    Update instrumentation test step config; fix test
    Resolves: DCMAW-6087

--- app/src/androidTest/java/uk/gov/onelogin/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/uk/gov/onelogin/ExampleInstrumentedTest.kt
@@ -6,7 +6,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
@@ -14,6 +14,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.onelogin", appContext.packageName)
+        assertEquals("uk.gov.onelogin", appContext.packageName)
     }
 }
