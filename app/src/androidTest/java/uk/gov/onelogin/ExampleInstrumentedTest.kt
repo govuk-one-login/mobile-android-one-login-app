547ff09437720a0115f336ddddfa71606768445d
Chris Gedrim <christopher.gedrim@digital.cabinet-office.gov.uk>
2023-09-05 12:36:53 +0100

    Refactor exampl einstrumentation test
    Resolves: DCMAW-6087

--- app/src/androidTest/java/uk/gov/onelogin/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/uk/gov/onelogin/ExampleInstrumentedTest.kt
@@ -14,6 +14,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("uk.gov.onelogin", appContext.packageName)
+        assertEquals("uk.gov.onelogin.staging", appContext.packageName)
     }
 }
