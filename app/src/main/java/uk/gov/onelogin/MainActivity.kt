b163af008da78d645491719099eada8e1406b7f2
dmubiligi <128810121+de1phine@users.noreply.github.com>
2023-10-02 14:34:56 +0100

    Remove comments, unused imports and unused values

--- app/src/main/java/uk/gov/onelogin/MainActivity.kt
+++ app/src/main/java/uk/gov/onelogin/MainActivity.kt
@@ -2,14 +2,7 @@ package uk.gov.onelogin
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
