15e60c55201d7de1d6c1a4de4b92019d2fe7506f
dmubiligi <128810121+de1phine@users.noreply.github.com>
2023-10-05 16:01:06 +0100

    Replace xml with compose
    Resolves: DCMAW-6345
    
    Co-authored-by: Chuddington <6558350+chuddington@users.noreply.github.com>

--- app/src/main/java/uk/gov/onelogin/MainActivity.kt
+++ app/src/main/java/uk/gov/onelogin/MainActivity.kt
@@ -9,7 +9,21 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.browser.customtabs.CustomTabsIntent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
@@ -17,17 +31,34 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
 
         installSplashScreen().apply {}
-
-        val signInButton = findViewById<Button>(R.id.signin,)
-            .setOnClickListener {
-                val url = "https://gov.uk"
-                val intent = CustomTabsIntent.Builder()
-                    .build()
-                intent.launchUrl(this@MainActivity, Uri.parse(url))
+        setContent { 
+            GdsTheme {
+                Column(
+                    modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Button(onClick = {
+                        val url = "https://gov.uk"
+                        val intent = CustomTabsIntent.Builder()
+                            .build()
+                        intent.launchUrl(this@MainActivity, Uri.parse(url))
+                    }) {
+                        Text(text = "Sign In")
+                    }
+                }
             }
+        }
+
+//        val signInButton = findViewById<Button>(R.id.signin,)
+//            .setOnClickListener {
+//                val url = "https://gov.uk"
+//                val intent = CustomTabsIntent.Builder()
+//                    .build()
+//                intent.launchUrl(this@MainActivity, Uri.parse(url))
+//            }
 
     }
 }
