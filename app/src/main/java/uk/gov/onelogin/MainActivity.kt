5eb4aa53e82e98c9daf2eb5de672ba809217a56b
Chris Gedrim <christopher.gedrim@digital.cabinet-office.gov.uk>
2023-11-11 20:34:04 +0000

    Use Material 3 GdsTheme
    Resolves: DCMAW-6344

--- app/src/main/java/uk/gov/onelogin/MainActivity.kt
+++ app/src/main/java/uk/gov/onelogin/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
-import uk.gov.android.ui.theme.GdsTheme
+import uk.gov.android.ui.theme.m3.GdsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
