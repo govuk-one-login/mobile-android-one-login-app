79b4adbeb68063d6d95ecb779255d350a5c48dc3
Chris Gedrim <christopher.gedrim@digital.cabinet-office.gov.uk>
2023-11-13 15:21:05 +0000

    Add online check to Token Exchange
    Resolves: DCMAW-6886

--- app/src/test/java/uk/gov/onelogin/network/auth/TokenExchangeTest.kt
+++ app/src/test/java/uk/gov/onelogin/network/auth/TokenExchangeTest.kt
@@ -1,11 +1,40 @@
 package uk.gov.onelogin.network.auth
 
+import org.junit.Before
 import org.junit.Test
 import uk.gov.onelogin.network.auth.TokenExchange.Companion.TokenExchangeCodeArgError
+import uk.gov.onelogin.network.auth.TokenExchange.Companion.TokenExchangeOfflineError
+import uk.gov.onelogin.network.utils.IOnlineChecker
 
 class TokenExchangeTest {
-    @Test(expected=TokenExchangeCodeArgError::class)
-    fun `throws an InvalidArgumentException when an empty code is passed`() {
-        TokenExchange("")
+    class OnlineCheckerStub: IOnlineChecker {
+        var online: Boolean = true
+
+        override fun isOnline(): Boolean = online
+    }
+
+    private val stubOnlineChecker = OnlineCheckerStub()
+
+    @Before
+    fun setup() {
+        stubOnlineChecker.online = true
+    }
+
+    @Test(expected = TokenExchangeCodeArgError::class)
+    fun `throws a TokenExchangeCodeArgError when an empty code is passed`() {
+        TokenExchange(
+            code = "",
+            onlineChecker = stubOnlineChecker
+        )
+    }
+
+    @Test(expected = TokenExchangeOfflineError::class)
+    fun `throws a TokenExchangeOfflineError when the device is offline`() {
+        stubOnlineChecker.online = false
+
+        TokenExchange(
+            code = "aCode",
+            onlineChecker = stubOnlineChecker
+        )
     }
 }
