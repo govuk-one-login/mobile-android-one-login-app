117dfb2682e844e0da4b1d402895e456520ee1bc
Chris Gedrim <christopher.gedrim@digital.cabinet-office.gov.uk>
2023-11-13 15:21:05 +0000

    Add online check to Token Exchange
    Resolves: DCMAW-6886

--- app/src/main/java/uk/gov/onelogin/network/auth/TokenExchange.kt
+++ app/src/main/java/uk/gov/onelogin/network/auth/TokenExchange.kt
@@ -1,15 +1,23 @@
 package uk.gov.onelogin.network.auth
 
+import uk.gov.onelogin.network.utils.IOnlineChecker
+
 class TokenExchange constructor(
-    private val code: String
+    code: String,
+    onlineChecker: IOnlineChecker
 ) {
     init {
         if (code.isEmpty()) {
             throw TokenExchangeCodeArgError("Code should be a non-empty string")
         }
+
+        if (!onlineChecker.isOnline()) {
+            throw TokenExchangeOfflineError("The device appears to be offline")
+        }
     }
 
     companion object {
         class TokenExchangeCodeArgError constructor(message: String): Error(message)
+        class TokenExchangeOfflineError constructor(message: String): Error(message)
     }
 }
