a9e8e35adf0dbad84deff0204ea53d52d3fb553f
MohamedHassan101 <mohamed.hassan@digital.cabinet-office.gov.uk>
2023-11-13 16:55:30 +0000

    Add test for TokenExchangeServerError

--- app/src/test/java/uk/gov/onelogin/network/auth/TokenExchangeTest.kt
+++ app/src/test/java/uk/gov/onelogin/network/auth/TokenExchangeTest.kt
@@ -1,9 +1,19 @@
 package uk.gov.onelogin.network.auth
 
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.mock.MockEngine
+import io.ktor.client.engine.mock.respond
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.Url
+import io.ktor.http.headersOf
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
 import uk.gov.onelogin.network.auth.TokenExchange.Companion.TokenExchangeCodeArgError
 import uk.gov.onelogin.network.auth.TokenExchange.Companion.TokenExchangeOfflineError
+import uk.gov.onelogin.network.auth.TokenExchange.Companion.TokenExchangeServerError
 import uk.gov.onelogin.network.utils.IOnlineChecker
 
 class TokenExchangeTest {
@@ -15,6 +25,17 @@ class TokenExchangeTest {
 
     private val stubOnlineChecker = OnlineCheckerStub()
 
+    private val redirectUrl = Url("https://example.com/redirect")
+    private val url = Url("https://example.com/token")
+    private val httpClient = HttpClient(MockEngine{
+        respond(
+            content = "",
+            status = HttpStatusCode.InternalServerError,
+            headers = headersOf(
+                HttpHeaders.ContentType, ContentType.Application.Json.toString()
+            )
+        )
+    })
     @Before
     fun setup() {
         stubOnlineChecker.online = true
@@ -24,7 +45,10 @@ class TokenExchangeTest {
     fun `throws a TokenExchangeCodeArgError when an empty code is passed`() {
         TokenExchange(
             code = "",
-            onlineChecker = stubOnlineChecker
+            onlineChecker = stubOnlineChecker,
+            redirectUrl = redirectUrl,
+            url = url,
+            httpClient = httpClient
         )
     }
 
@@ -34,7 +58,24 @@ class TokenExchangeTest {
 
         TokenExchange(
             code = "aCode",
-            onlineChecker = stubOnlineChecker
+            onlineChecker = stubOnlineChecker,
+            redirectUrl = redirectUrl,
+            url = url,
+            httpClient = httpClient
         )
+
+
+    }
+    @Test(expected = TokenExchangeServerError::class)
+    fun `throws a TokenExchangeServerError when a 500 range error is received`() = runBlocking {
+        TokenExchange(
+            code = "aCode",
+            onlineChecker = stubOnlineChecker,
+            redirectUrl = redirectUrl,
+            url = url,
+            httpClient = httpClient
+        ).send()
+
+
     }
 }
