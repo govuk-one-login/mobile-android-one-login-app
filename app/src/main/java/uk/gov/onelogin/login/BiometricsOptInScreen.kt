f2832865cb615e6e3146bab28525ce6c6de803e6
abradbury <18612852+alex-bradbury@users.noreply.github.com>
2024-02-21 16:35:22 +0000

    Add necessary strings in english and welsh, also start to build biometrics opt-in screen with vector
    Resolves: DCMAW-7849

--- app/src/main/java/uk/gov/onelogin/login/BiometricsOptInScreen.kt
+++ app/src/main/java/uk/gov/onelogin/login/BiometricsOptInScreen.kt
@@ -6,14 +6,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import uk.gov.android.ui.components.GdsHeading
+import uk.gov.android.ui.components.GdsVectorImage
+import uk.gov.android.ui.components.HeadingParameters
+import uk.gov.android.ui.components.HeadingSize
+import uk.gov.android.ui.components.VectorImageParameters
 import uk.gov.android.ui.theme.GdsTheme
+import uk.gov.onelogin.R
 
 @Composable
 fun BiometricsOptInScreen() {
@@ -26,10 +32,21 @@ fun BiometricsOptInScreen() {
                 .fillMaxSize()
                 .verticalScroll(rememberScrollState())
         ) {
-            Text(
-                text = "Placeholder",
-                fontWeight = FontWeight.Bold
-            )
+            Column {
+                GdsVectorImage(
+                    VectorImageParameters(
+                        image = R.drawable.bio_opt_in,
+                        scale = ContentScale.Fit
+                    )
+                )
+                GdsHeading(
+                    HeadingParameters(
+                        text = R.string.app_enableBiometricsTitle,
+                        size = HeadingSize.H1(),
+                        textAlign = TextAlign.Center
+                    )
+                )
+            }
         }
     }
 }
