a646065783fa532ef24684bfa14d99a5251732b0
Flavius <flavius.mester@digital.cabinet-office.gov.uk>
2024-04-09 11:28:59 +0100

    add SaveToSecureStoreTest

---
+++ app/src/test/java/uk/gov/onelogin/tokens/usecases/SaveToSecureStoreTest.kt
@@ -0,0 +1,52 @@
+package uk.gov.onelogin.tokens.usecases
+
+import androidx.fragment.app.FragmentActivity
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.kotlin.any
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import uk.gov.android.securestore.SecureStorageError
+import uk.gov.android.securestore.SecureStore
+import uk.gov.onelogin.extensions.CoroutinesTestExtension
+
+@ExtendWith(CoroutinesTestExtension::class)
+class SaveToSecureStoreTest {
+    private lateinit var useCase: SaveToSecureStore
+    private val mockFragmentActivity: FragmentActivity = mock()
+    private val mockSecureStore: SecureStore = mock()
+
+    private val expectedStoreKey: String = "key"
+    private val expectedStoreValue: String = "value"
+
+    @BeforeEach
+    fun setUp() {
+        useCase = SaveToSecureStoreImpl(mockSecureStore)
+    }
+
+    @Test
+    fun `does not throw - when SecureStorageError thrown`() = runTest {
+        whenever(mockSecureStore.upsert(any(), any(), any())).thenThrow(
+            SecureStorageError(Exception("Some error"))
+        )
+
+        assertDoesNotThrow {
+            useCase.invoke(mockFragmentActivity, expectedStoreKey, expectedStoreValue)
+        }
+    }
+
+    @Test
+    fun `saves value successfully`() = runTest {
+        useCase.invoke(mockFragmentActivity, expectedStoreKey, expectedStoreValue)
+
+        verify(mockSecureStore).upsert(
+            expectedStoreKey,
+            expectedStoreValue,
+            mockFragmentActivity
+        )
+    }
+}
