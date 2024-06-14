a744e1e22756d45446f087db5656f0a3af93cc54
Chuddington <6558350+Chuddington@users.noreply.github.com>
2024-06-14 15:52:36 +0100

    Use a single hardware profile
    - Make the configuration of managed device hardware profiles more static
      by changing gradle to refer to a commit file's contents. Developers
      update this `config/managedDeviceHardwareProfiles` list by running the
      `.sh/getAllHardwareProfileNames` then remove the unwanted profiles.
    - Remove `--max-workers` parameter from workflow call to run
      instrumentation tests. This allows gradle to use all applicable
      threads, whilst maintaining the shard limit of 3.
    - Remove workflow setup step for gradle managed device. This is a
      dependency of running the instrumentation tests and as a task with
      state external to gradle, runs every time.

--- buildLogic/plugins/src/main/kotlin/uk/gov/onelogin/emulator-config.gradle.kts
+++ buildLogic/plugins/src/main/kotlin/uk/gov/onelogin/emulator-config.gradle.kts
@@ -3,11 +3,11 @@ package uk.gov.onelogin
 import com.android.build.gradle.BaseExtension
 import java.io.BufferedReader
 import java.io.ByteArrayOutputStream
+import java.io.FileReader
 import java.io.StringReader
 import org.gradle.kotlin.dsl.configure
 import uk.gov.onelogin.emulator.SystemImageSource
 import uk.gov.onelogin.emulator.SystemImageSource.GOOGLE_ATD
-import uk.gov.onelogin.emulator.SystemImageSource.GOOGLE_PLAYSTORE
 import uk.gov.extensions.BaseExtensions.generateDeviceConfigurations
 import uk.gov.extensions.BaseExtensions.generateGetHardwareProfilesTask
 
@@ -15,12 +15,15 @@ plugins {
     id("kotlin-android")
 }
 
-private val _hardwareProfileFilter: (String) -> Boolean = {
-    it.contains("pixel 5", ignoreCase = true)
-}
 private val _systemImageSources = listOf(
-    GOOGLE_ATD,
-    GOOGLE_PLAYSTORE
+    GOOGLE_ATD
+)
+val managedDeviceHardwareProfiles: Provider<List<String>> by rootProject.extra(
+    rootProject.provider {
+        FileReader(rootProject.file("config/managedDeviceHardwareProfiles"))
+            .readLines()
+            .filter { !it.trim().startsWith("#") } // remove comment lines
+    }
 )
 
 /**
@@ -45,30 +48,11 @@ configure<BaseExtension> {
      * device ID. Therefore, this should be an [IntRange] with a single entry until fixed.
      */
     val managedApiLevels: IntRange by project.extra((30..30))
-    val hardwareProfileFilter: (String) -> Boolean by project.extra(_hardwareProfileFilter)
     val systemImageSources: List<SystemImageSource> by project.extra(_systemImageSources)
-
-    val consoleOutputStream = ByteArrayOutputStream()
-    val hardwareProfilesTask = generateGetHardwareProfilesTask(project, consoleOutputStream)
-
-    /**
-     * Call the hardware profiles task within the gradle configuration stage for the sake of
-     * building out the various hardware profiles.
-     */
-    exec {
-        commandLine = hardwareProfilesTask.get().commandLine
-        args = hardwareProfilesTask.get().args
-        standardOutput = consoleOutputStream
-    }
-
-    val hardwareProfileStrings = BufferedReader(
-        StringReader(String(consoleOutputStream.toByteArray()))
-    )
-        .readLines()
 
     generateDeviceConfigurations(
         apiLevelRange = managedApiLevels,
-        hardwareProfileStrings = hardwareProfileStrings.filter(hardwareProfileFilter),
+        hardwareProfileStrings = managedDeviceHardwareProfiles.get(),
         systemImageSources = systemImageSources
     )
 }
