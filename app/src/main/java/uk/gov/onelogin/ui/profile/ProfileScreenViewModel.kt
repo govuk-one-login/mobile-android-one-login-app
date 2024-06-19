fa73d19362d68ca37c5743684272aadcde0d0fa8
Flavius <flavius.mester@digital.cabinet-office.gov.uk>
2024-06-19 11:54:30 +0100

    Add email to the ProfileScreen

---
+++ app/src/main/java/uk/gov/onelogin/ui/profile/ProfileScreenViewModel.kt
@@ -0,0 +1,13 @@
+package uk.gov.onelogin.ui.profile
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import uk.gov.onelogin.tokens.usecases.GetEmail
+
+@HiltViewModel
+class ProfileScreenViewModel @Inject constructor(
+    getEmail: GetEmail
+) : ViewModel() {
+    val email = getEmail() ?: ""
+}
