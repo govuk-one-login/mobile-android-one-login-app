8c808fff828d714905e865bb046133cd3ea545bc
ThomasIent <thomas.ient@digital.cabinet-office.gov.uk>
2024-09-06 09:17:47 +0100

    feat: Add SingleChoice
    Add SingleChoice as a way of reducing possible debounce
    Add SingleChoiceTest to check a second interaction can't happen

---
+++ app/src/test/java/uk/gov/onelogin/optin/domain/SingleChoiceTest.kt
@@ -0,0 +1,36 @@
+package uk.gov.onelogin.optin.domain
+
+import kotlinx.coroutines.test.runTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SingleChoiceTest {
+    private lateinit var choice: SingleChoice
+
+    @BeforeTest
+    fun setUp() {
+        choice = SingleChoice()
+    }
+
+    @Test
+    fun `option block is called`() = runTest {
+        // Given a SingleChoice
+        var actual = false
+        // When calling choose
+        choice.choose { actual = true }
+        // Then the options code block is called
+        assertEquals(expected = true, actual)
+    }
+
+    @Test
+    fun `option block is not called a second time`() = runTest {
+        // Given a SingleChoice
+        var actual = false
+        // When calling choose twice
+        choice.choose {  }
+        choice.choose { actual = true }
+        // Then the second options code block is not called
+        assertEquals(expected = false, actual)
+    }
+}
