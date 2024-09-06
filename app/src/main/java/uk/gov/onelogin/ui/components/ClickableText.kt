0d0d0306d99705cceafc87afb1ddc5cafbd1050f
ThomasIent <thomas.ient@digital.cabinet-office.gov.uk>
2024-09-06 09:18:45 +0100

    feat: Add ClickableText
    Add ClickableText composable that takes an inlineContent for passing through to BasicText
    This allows SVGs to be put inline with text in a ClickableText composable
    Otherwise identical to the standard ClickableText

---
+++ app/src/main/java/uk/gov/onelogin/ui/components/ClickableText.kt
@@ -0,0 +1,56 @@
+package uk.gov.onelogin.ui.components
+
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.foundation.text.InlineTextContent
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextOverflow
+
+/**
+ * Adds inlineContent Map to ClickableText
+ * identical otherwise to the Android provided ClickableText
+ * inlineContent Map is how the Text Composable does inline content
+ */
+@SuppressWarnings("squid:S107") // Suppressing due to matching Android
+@Composable
+fun ClickableText(
+    text: AnnotatedString,
+    modifier: Modifier = Modifier,
+    style: TextStyle = TextStyle.Default,
+    softWrap: Boolean = true,
+    overflow: TextOverflow = TextOverflow.Clip,
+    maxLines: Int = Int.MAX_VALUE,
+    onTextLayout: (TextLayoutResult) -> Unit = {},
+    inlineContent: Map<String, InlineTextContent> = mapOf(),
+    onClick: (Int) -> Unit
+) {
+    val layoutResult = remember { mutableStateOf<TextLayoutResult?>(null) }
+    val pressIndicator = Modifier.pointerInput(onClick) {
+        detectTapGestures { pos ->
+            layoutResult.value?.let { layoutResult ->
+                onClick(layoutResult.getOffsetForPosition(pos))
+            }
+        }
+    }
+
+    BasicText(
+        text = text,
+        modifier = modifier.then(pressIndicator),
+        style = style,
+        softWrap = softWrap,
+        overflow = overflow,
+        maxLines = maxLines,
+        onTextLayout = {
+            layoutResult.value = it
+            onTextLayout(it)
+        },
+        inlineContent = inlineContent,
+    )
+}
