84a5867f8a91df0fe1ef4f6bf91a2e4f373420ae
ThomasIent <thomas.ient@digital.cabinet-office.gov.uk>
2024-09-12 12:47:05 +0100

    feat: Fix duplicate hilt provisions
    Add IODispatcherQualifier to distinguish between the IO and the Default Dispatcher
    The IO Dispatcher is provided vai hilt in the optin package
    The Default Dispatcher is provided vai hilt in the wallet project
    
    Resolves: DCMAW-8582

--- app/src/main/java/uk/gov/onelogin/optin/data/FirebaseAnalyticsOptInSource.kt
+++ app/src/main/java/uk/gov/onelogin/optin/data/FirebaseAnalyticsOptInSource.kt
@@ -7,9 +7,11 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import uk.gov.onelogin.optin.domain.model.AnalyticsOptInState
 import uk.gov.onelogin.optin.domain.source.OptInRemoteSource
+import uk.gov.onelogin.optin.ui.IODispatcherQualifier
 
 class FirebaseAnalyticsOptInSource @Inject constructor(
     private val analytics: FirebaseAnalytics,
+    @IODispatcherQualifier
     private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : OptInRemoteSource {
 
