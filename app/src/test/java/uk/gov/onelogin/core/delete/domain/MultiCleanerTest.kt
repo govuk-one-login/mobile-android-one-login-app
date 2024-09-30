bb9b4903964796e1b8143da8424213d45111f534
ThomasIent <thomas.ient@digital.cabinet-office.gov.uk>
2024-09-30 15:56:13 +0100

    feat: Add Cleaner
    Add Cleaner a type for marking classes that need data cleaned on sign out
    Add MultiCleaner class for running cleaners' clean method simultaneously and independently
    Add MultiCleanerTest to test MultiCleaner
    
    Resolves: 10305

---
+++ app/src/test/java/uk/gov/onelogin/core/delete/domain/MultiCleanerTest.kt
@@ -0,0 +1,51 @@
+package uk.gov.onelogin.core.delete.domain
+
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertTrue
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+
+@ExperimentalCoroutinesApi
+class MultiCleanerTest {
+    private val dispatcher = StandardTestDispatcher()
+    private lateinit var cleaner: MultiCleaner
+
+    @BeforeTest
+    fun setUp() {
+        Dispatchers.setMain(dispatcher)
+    }
+
+    @AfterTest
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `clean runs all sub-cleaners' clean`() = runTest {
+        // Given MultiCleaner
+        var actual0 = false
+        var actual1 = false
+        var actual2 = false
+        var actual3 = false
+        cleaner = MultiCleaner(
+            dispatcher,
+            Cleaner { actual0 = true; Result.success(Unit) },
+            Cleaner { actual1 = true; Result.success(Unit) },
+            Cleaner { actual2 = true; Result.success(Unit) },
+            Cleaner { actual3 = true; Result.success(Unit) }
+        )
+        // When calling clean
+        cleaner.clean()
+        // Then run all sub-cleaners' clean
+        assertTrue(actual0)
+        assertTrue(actual1)
+        assertTrue(actual2)
+        assertTrue(actual3)
+    }
+}
