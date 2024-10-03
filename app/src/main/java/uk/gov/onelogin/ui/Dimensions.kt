8c50564baa811dfb0197d54750441bcf582bfb68
Bianca Mihaila <bianca.mihaila@digital.cabinet-office.gov.uk>
2024-10-03 10:46:11 +0100

    feat: Add new dimensions needed for ReAuthErrorScreen
    Resolves: DCMAW-9670

--- app/src/main/java/uk/gov/onelogin/ui/Dimensions.kt
+++ app/src/main/java/uk/gov/onelogin/ui/Dimensions.kt
@@ -5,3 +5,8 @@ import androidx.compose.ui.unit.dp
 val buttonPaddingVertical = 13.dp
 val buttonPaddingHorizontal = 16.dp
 val buttonElevation = 0.dp
+
+// Re-Auth Error Screen
+val listContentPadding = 12.dp
+val bulletPointIndentation = 10.dp
+val bulletPointTextPadding = 20.dp
