10017cda02d8c1db6ef175c80e84ce61c2f9036d
Alex Bradbury <alex.bradbury@digital.cabinet-office.gov.uk>
2024-10-04 17:00:56 +0100

    feat: Add facility to delete persistent id in dev menu Also make sure login is not sending an empty persistent id
    Resolves: DCMAW-9193

--- app/src/main/java/uk/gov/onelogin/developer/tabs/tokens/TokenTabScreenViewModel.kt
+++ app/src/main/java/uk/gov/onelogin/developer/tabs/tokens/TokenTabScreenViewModel.kt
@@ -1,15 +1,44 @@
 package uk.gov.onelogin.developer.tabs.tokens
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.launch
+import uk.gov.onelogin.tokens.Keys
+import uk.gov.onelogin.tokens.usecases.GetPersistentId
+import uk.gov.onelogin.tokens.usecases.SaveToOpenSecureStore
 import uk.gov.onelogin.tokens.usecases.SaveTokenExpiry
 
 @HiltViewModel
 class TokenTabScreenViewModel @Inject constructor(
-    private val saveTokenExpiry: SaveTokenExpiry
+    private val saveTokenExpiry: SaveTokenExpiry,
+    private val saveToOpenSecureStore: SaveToOpenSecureStore,
+    private val getPersistentId: GetPersistentId
 ) : ViewModel() {
+    private val _persistentId = mutableStateOf("")
+    val persistentId: State<String>
+        get() = _persistentId
+
+    init {
+        viewModelScope.launch {
+            _persistentId.value = getPersistentId() ?: ""
+        }
+    }
+
     fun resetAccessToken() {
         saveTokenExpiry(System.currentTimeMillis() - 1)
+    }
+
+    fun resetPersistentID() {
+        viewModelScope.launch {
+            saveToOpenSecureStore(
+                Keys.PERSISTENT_ID_KEY,
+                ""
+            )
+            _persistentId.value = getPersistentId() ?: ""
+        }
     }
 }
