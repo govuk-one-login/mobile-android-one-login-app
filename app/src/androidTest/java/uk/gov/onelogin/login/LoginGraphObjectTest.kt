a9dea118964bd0501baf1c571069951f6ac54202
Bianca Mihaila <bianca.mihaila@digital.cabinet-office.gov.uk>
2024-10-14 15:14:21 +0100

    test: Mock AppInfoService to check reason for failing tests
    Resolves: DCMAW-10332

--- app/src/androidTest/java/uk/gov/onelogin/login/LoginGraphObjectTest.kt
+++ app/src/androidTest/java/uk/gov/onelogin/login/LoginGraphObjectTest.kt
@@ -4,23 +4,51 @@ import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.test.filters.FlakyTest
+import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
 import javax.inject.Inject
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
 import uk.gov.android.onelogin.R
 import uk.gov.onelogin.MainActivity
+import uk.gov.onelogin.appinfo.AppInfoApiModule
+import uk.gov.onelogin.appinfo.apicall.domain.model.AppInfoData
+import uk.gov.onelogin.appinfo.service.domain.AppInfoService
+import uk.gov.onelogin.appinfo.service.domain.model.AppInfoServiceState
 import uk.gov.onelogin.e2e.controller.TestCase
 import uk.gov.onelogin.navigation.Navigator
 
 @HiltAndroidTest
+@UninstallModules(AppInfoApiModule::class)
 class LoginGraphObjectTest : TestCase() {
+    private val data = AppInfoData(
+        apps = AppInfoData.App(
+            AppInfoData.AppInfo(
+                minimumVersion = "0.0.0",
+                releaseFlags = AppInfoData.ReleaseFlags(
+                    true,
+                    true,
+                    true
+                ),
+                available = true,
+                featureFlags = AppInfoData.FeatureFlags(true)
+            )
+        )
+    )
+
     @get:Rule(order = 3)
     val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     @Inject
     lateinit var navigator: Navigator
+
+    @BindValue
+    val appInfoService: AppInfoService = mock()
 
     @Before
     fun setup() {
@@ -29,7 +57,8 @@ class LoginGraphObjectTest : TestCase() {
 
     @FlakyTest
     @Test
-    fun loginGraph_SignInError() {
+    fun loginGraph_SignInError() = runTest {
+        whenever(appInfoService.get()).thenReturn(AppInfoServiceState.RemoteSuccess(data))
         composeTestRule.activityRule.scenario.onActivity {
             navigator.navigate(LoginRoutes.SignInError)
         }
