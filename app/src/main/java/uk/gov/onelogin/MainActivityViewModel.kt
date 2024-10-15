d9773def5fe208a0731ff715372009344bfd5dc7
ThomasIent <thomas.ient@digital.cabinet-office.gov.uk>
2024-10-15 17:40:01 +0100

    fix: Synchronise Analytics Preference
    Update MainActivityViewModel to synchronise the Analytics preference on App start to ensure any changes to the shared preferences outside of the app are applied
    Update MainActivityViewModelTest to match
    The problem of outside changes to the SharedPreferences shouldn't come up in standard usage, but is evident when deleting data often during testing
    
    Resolves: 9613

--- app/src/main/java/uk/gov/onelogin/MainActivityViewModel.kt
+++ app/src/main/java/uk/gov/onelogin/MainActivityViewModel.kt
@@ -3,17 +3,21 @@ package uk.gov.onelogin
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.launch
 import uk.gov.onelogin.login.LoginRoutes
 import uk.gov.onelogin.login.biooptin.BiometricPreference
 import uk.gov.onelogin.login.biooptin.BiometricPreferenceHandler
 import uk.gov.onelogin.navigation.Navigator
+import uk.gov.onelogin.optin.domain.repository.AnalyticsOptInRepository
 import uk.gov.onelogin.repositiories.TokenRepository
 import uk.gov.onelogin.tokens.usecases.AutoInitialiseSecureStore
 
 @HiltViewModel
 class MainActivityViewModel @Inject constructor(
+    private val analyticsOptInRepo: AnalyticsOptInRepository,
     private val bioPrefHandler: BiometricPreferenceHandler,
     private val tokenRepository: TokenRepository,
     private val navigator: Navigator,
@@ -22,6 +26,13 @@ class MainActivityViewModel @Inject constructor(
 
     init {
         autoInitialiseSecureStore()
+    }
+
+    override fun onStart(owner: LifecycleOwner) {
+        super.onStart(owner)
+        viewModelScope.launch {
+            analyticsOptInRepo.synchronise()
+        }
     }
 
     override fun onPause(owner: LifecycleOwner) {
