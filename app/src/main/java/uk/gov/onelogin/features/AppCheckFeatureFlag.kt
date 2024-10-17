80d3241ba63b1e0fd1950c8139a8fe4126b35242
Alex Bradbury <alex.bradbury@digital.cabinet-office.gov.uk>
2024-10-17 12:53:45 +0100

    fix: feature flag names in dev menu
    Resolves: DCMAW-10327

--- app/src/main/java/uk/gov/onelogin/features/AppCheckFeatureFlag.kt
+++ app/src/main/java/uk/gov/onelogin/features/AppCheckFeatureFlag.kt
@@ -2,6 +2,6 @@ package uk.gov.onelogin.features
 
 import uk.gov.android.features.FeatureFlag
 
-enum class AppCheckFeatureFlag(override val id: String = "appCheckEnabled") : FeatureFlag {
+enum class AppCheckFeatureFlag(override val id: String = "App Check Enabled") : FeatureFlag {
     ENABLED
 }
--- app/src/main/java/uk/gov/onelogin/features/StsFeatureFlag.kt
+++ app/src/main/java/uk/gov/onelogin/features/StsFeatureFlag.kt
@@ -2,6 +2,6 @@ package uk.gov.onelogin.features
 
 import uk.gov.android.features.FeatureFlag
 
-enum class StsFeatureFlag(override val id: String = this.toString()) : FeatureFlag {
+enum class StsFeatureFlag(override val id: String = "STS Endpoint") : FeatureFlag {
     STS_ENDPOINT
 }
