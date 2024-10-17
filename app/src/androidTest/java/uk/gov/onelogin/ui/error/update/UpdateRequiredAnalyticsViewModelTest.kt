5d1ed126602ba1bdd66148a569f816783dabf347
ThomasIent <thomas.ient@digital.cabinet-office.gov.uk>
2024-10-17 20:55:02 +0100

    feat: Update UpdateRequiredScreen to use analytics
    Update UpdateRequiredScreen to use UpdateRequiredAnalyticsViewModel
    Update UpdateRequiredScreenTest to verify analytics calls
    
    Resolves: 9613

--- app/src/androidTest/java/uk/gov/onelogin/ui/error/update/UpdateRequiredAnalyticsViewModelTest.kt
+++ app/src/androidTest/java/uk/gov/onelogin/ui/error/update/UpdateRequiredAnalyticsViewModelTest.kt
@@ -1,4 +1,4 @@
-package uk.gov.onelogin.ui.update
+package uk.gov.onelogin.ui.error.update
 
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
--- app/src/main/java/uk/gov/onelogin/ui/error/update/UpdateRequiredErrorScreen.kt
+++ app/src/main/java/uk/gov/onelogin/ui/error/update/UpdateRequiredErrorScreen.kt
@@ -1,5 +1,6 @@
 package uk.gov.onelogin.ui.error.update
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -7,6 +8,9 @@ import androidx.compose.ui.tooling.preview.PreviewFontScale
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LifecycleEventEffect
+import androidx.navigation.compose.rememberNavController
 import uk.gov.android.onelogin.R
 import uk.gov.android.ui.components.R as UiR
 import uk.gov.android.ui.components.content.GdsContentText
@@ -14,14 +18,25 @@ import uk.gov.android.ui.pages.LandingPage
 import uk.gov.android.ui.pages.LandingPageParameters
 import uk.gov.android.ui.theme.m3.GdsTheme
 import uk.gov.android.ui.theme.smallPadding
+import uk.gov.onelogin.navigation.closeApp
 
 @Composable
 fun UpdateRequiredScreen(
     viewModel: UpdateRequiredErrorViewModel = hiltViewModel()
 ) {
+    val analytics: UpdateRequiredAnalyticsViewModel = hiltViewModel()
+    val navController = rememberNavController()
     UpdateRequiredBody(
-        onPrimary = { viewModel.updateApp() }
+        onPrimary = {
+            viewModel.updateApp()
+            analytics.trackAppUpdate()
+        }
     )
+    BackHandler {
+        analytics.trackBackButton()
+        navController.closeApp()
+    }
+    LifecycleEventEffect(Lifecycle.Event.ON_RESUME) { analytics.trackUpdateRequiredView() }
 }
 
 @Composable
