c0af55a41e68819847708bd7f69329627ecf64ca
ThomasIent <thomas.ient@digital.cabinet-office.gov.uk>
2024-10-17 20:41:22 +0100

    feat: Add closeApp extension function
    Update NavControllerEtx to include closeApp extension function
    Add NavControllerExtTest to test the closeApp function as much as possible
    
    Resolves: 9613

--- app/src/main/java/uk/gov/onelogin/navigation/NavControllerExt.kt
+++ app/src/main/java/uk/gov/onelogin/navigation/NavControllerExt.kt
@@ -1,8 +1,16 @@
 package uk.gov.onelogin.navigation
 
+import android.app.Activity
 import androidx.navigation.NavHostController
 
 fun NavHostController.hasPreviousBackStack() = this.previousBackStackEntry != null
 
 fun NavHostController.navigateSingleTopTo(route: String) =
     this.navigate(route) { launchSingleTop = true }
+
+fun NavHostController.closeApp() {
+    while (hasPreviousBackStack()) {
+        popBackStack()
+    }
+    (context as? Activity)?.finish()
+}
