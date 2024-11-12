5bc0e6e1cb4c4ad2a3d2e36058edef64d0a983c9
ThomasIent <thomas.ient@digital.cabinet-office.gov.uk>
2024-11-12 13:31:44 +0000

    feat: Add Wallet FeatureFlag check on SignOut
    Update SignOutViewModel to include SignOutUIState property based on the Wallet FeatureFlag value
    Update SignOutViewModelTest to test property
    
    Resolves: 10331

--- app/src/test/java/uk/gov/onelogin/signOut/ui/SignOutViewModelTest.kt
+++ app/src/test/java/uk/gov/onelogin/signOut/ui/SignOutViewModelTest.kt
@@ -1,6 +1,7 @@
 package uk.gov.onelogin.signOut.ui
 
 import androidx.fragment.app.FragmentActivity
+import kotlin.test.assertEquals
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.BeforeEach
@@ -9,8 +10,10 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
+import uk.gov.android.features.FeatureFlags
 import uk.gov.onelogin.extensions.CoroutinesTestExtension
 import uk.gov.onelogin.extensions.InstantExecutorExtension
+import uk.gov.onelogin.features.WalletFeatureFlag
 import uk.gov.onelogin.login.LoginRoutes
 import uk.gov.onelogin.navigation.Navigator
 import uk.gov.onelogin.signOut.domain.SignOutError
@@ -25,13 +28,16 @@ class SignOutViewModelTest {
     private val mockNavigator: Navigator = mock()
     private val mockActivity: FragmentActivity = mock()
     private val mockSignOutUseCase: SignOutUseCase = mock()
+    private val featureFlags: FeatureFlags = mock()
 
     @BeforeEach
     fun setup() {
         viewModel = SignOutViewModel(
             mockNavigator,
-            mockSignOutUseCase
+            mockSignOutUseCase,
+            featureFlags
         )
+        whenever(featureFlags[WalletFeatureFlag.ENABLED]).then { true }
     }
 
     @Test
@@ -57,5 +63,19 @@ class SignOutViewModelTest {
         viewModel.goBack()
 
         verify(mockNavigator).goBack()
+    }
+
+    @Test
+    fun `uiState Wallet`() {
+        whenever(featureFlags[WalletFeatureFlag.ENABLED]).then { true }
+        val actual = viewModel.uiState
+        assertEquals(expected = SignOutUIState.Wallet, actual = actual)
+    }
+
+    @Test
+    fun `uiState No Wallet`() {
+        whenever(featureFlags[WalletFeatureFlag.ENABLED]).then { false }
+        val actual = viewModel.uiState
+        assertEquals(expected = SignOutUIState.NoWallet, actual = actual)
     }
 }
