45d5c07dc5b5de5f2a3affec8cfb1064e8e0d191
Alex Bradbury <alex.bradbury@digital.cabinet-office.gov.uk>
2024-11-17 21:23:11 +0000

    test: check back handler when not enabled
    Resolves: DCMAW-8924

--- app/src/androidTest/java/uk/gov/onelogin/ui/components/BackHandlerWithPopTest.kt
+++ app/src/androidTest/java/uk/gov/onelogin/ui/components/BackHandlerWithPopTest.kt
@@ -3,10 +3,12 @@ package uk.gov.onelogin.ui.components
 import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
+import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyNoInteractions
 import uk.gov.onelogin.TestCase
 import uk.gov.onelogin.navigation.Navigator
 import uk.gov.onelogin.navigation.NavigatorModule
@@ -34,6 +36,24 @@ class BackHandlerWithPopTest : TestCase() {
 
             assertTrue(backPressed)
             verify(navigator).goBack()
+        }
+    }
+
+    @Test
+    fun backBehaviourTestNotEnabled() {
+        var backPressed = false
+
+        composeTestRule.setContent {
+            BackHandlerWithPop(false) {
+                backPressed = true
+            }
+        }
+
+        composeTestRule.activityRule.scenario.onActivity {
+            it.onBackPressedDispatcher.onBackPressed()
+
+            assertFalse(backPressed)
+            verifyNoInteractions(navigator)
         }
     }
 }
