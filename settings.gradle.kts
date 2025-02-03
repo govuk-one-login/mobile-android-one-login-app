14792a06b9f034a93b434ed990ab4439c12f0437
Bianca Mihaila <bianca.mihaila@digital.cabinet-office.gov.uk>
2025-02-03 14:30:20 +0000

    build: Revert changed
    Resolves: DCMAW-11043

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,22 +18,6 @@ dependencyResolutionManagement {
             setupGithubCredentials()
         )
         maven(
-            "https://maven.pkg.github.com/govuk-one-login/mobile-wallet-android",
-            setupGithubCredentials()
-        )
-        maven(
-            "https://maven.pkg.github.com/govuk-one-login/mobile-android-cri-orchestrator",
-            setupGithubCredentials()
-        )
-        maven(
-            "https://maven.pkg.github.com/govuk-one-login/mobile-android-authentication",
-            setupGithubCredentials()
-        )
-        maven(
-            "https://maven.pkg.github.com/govuk-one-login/mobile-android-secure-store",
-            setupGithubCredentials()
-        )
-        maven(
             "https://maven.pkg.github.com/govuk-one-login/mobile-android-networking",
             setupGithubCredentials()
         )
