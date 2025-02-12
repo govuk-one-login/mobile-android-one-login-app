2394d64ceb789bb7b1c4cb85f3137a0b3e8277eb
Bianca Mihaila <bianca.mihaila@digital.cabinet-office.gov.uk>
2025-02-12 13:52:36 +0000

    feat: Change `CriCardFeatureFlag` to default to disabled
    Resolves: DCMAW-10011

--- app/src/androidTest/java/uk/gov/onelogin/ui/home/HomeScreenKtTest.kt
+++ app/src/androidTest/java/uk/gov/onelogin/ui/home/HomeScreenKtTest.kt
@@ -14,6 +14,8 @@ import org.junit.Before
 import org.junit.Test
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
+import uk.gov.android.features.FeatureFlags
+import uk.gov.android.features.InMemoryFeatureFlags
 import uk.gov.android.onelogin.R
 import uk.gov.logging.api.analytics.extensions.getEnglishString
 import uk.gov.logging.api.analytics.logging.AnalyticsLogger
@@ -26,9 +28,13 @@ import uk.gov.logging.api.v3dot1.model.ViewEvent
 import uk.gov.onelogin.TestCase
 import uk.gov.onelogin.core.analytics.AnalyticsModule
 import uk.gov.onelogin.ext.setupComposeTestRule
+import uk.gov.onelogin.features.CriCardFeatureFlag
+import uk.gov.onelogin.features.FeaturesModule
+import uk.gov.onelogin.features.WalletFeatureFlag
 
+@Suppress("ForbiddenComment")
 @HiltAndroidTest
-@UninstallModules(AnalyticsModule::class)
+@UninstallModules(AnalyticsModule::class, FeaturesModule::class)
 class HomeScreenKtTest : TestCase() {
     @BindValue
     var analytics: FirebaseAnalytics = Firebase.analytics
@@ -36,31 +42,11 @@ class HomeScreenKtTest : TestCase() {
     @BindValue
     var mockAnalyticsLogger: AnalyticsLogger = mock()
 
-    private val screenEvent = ViewEvent.Screen(
-        name = context.getEnglishString(R.string.app_home),
-        id = context.getEnglishString(R.string.home_page_id),
-        params = RequiredParameters(
-            taxonomyLevel2 = TaxonomyLevel2.HOME,
-            taxonomyLevel3 = TaxonomyLevel3.UNDEFINED
-        )
-    )
-
-    private val cardLinkEvent = TrackEvent.Link(
-        isExternal = true,
-        domain = context.getEnglishString(R.string.app_oneLoginCardLinkUrl),
-        text = context.getEnglishString(R.string.app_oneLoginCardLink),
-        params = RequiredParameters(
-            taxonomyLevel2 = TaxonomyLevel2.APP_SYSTEM,
-            taxonomyLevel3 = TaxonomyLevel3.UNDEFINED
-        )
-    )
-
-    private val backButtonEvent = TrackEvent.Icon(
-        text = context.getEnglishString(R.string.system_backButton),
-        params = RequiredParameters(
-            taxonomyLevel2 = TaxonomyLevel2.APP_SYSTEM,
-            taxonomyLevel3 = TaxonomyLevel3.UNDEFINED
-        )
+    // TODO: Remove this after `activeSession` has been added to the CriOrchestrator and test using the stub
+    //  provided
+    @BindValue
+    var featureFlags: FeatureFlags = InMemoryFeatureFlags(
+        setOf(WalletFeatureFlag.ENABLED, CriCardFeatureFlag.ENABLED)
     )
 
     @Before
@@ -109,4 +95,31 @@ class HomeScreenKtTest : TestCase() {
         verify(mockAnalyticsLogger).logEventV3Dot1(backButtonEvent)
         verify(mockAnalyticsLogger).logEventV3Dot1(cardLinkEvent)
     }
+
+    private val screenEvent = ViewEvent.Screen(
+        name = context.getEnglishString(R.string.app_home),
+        id = context.getEnglishString(R.string.home_page_id),
+        params = RequiredParameters(
+            taxonomyLevel2 = TaxonomyLevel2.HOME,
+            taxonomyLevel3 = TaxonomyLevel3.UNDEFINED
+        )
+    )
+
+    private val cardLinkEvent = TrackEvent.Link(
+        isExternal = true,
+        domain = context.getEnglishString(R.string.app_oneLoginCardLinkUrl),
+        text = context.getEnglishString(R.string.app_oneLoginCardLink),
+        params = RequiredParameters(
+            taxonomyLevel2 = TaxonomyLevel2.APP_SYSTEM,
+            taxonomyLevel3 = TaxonomyLevel3.UNDEFINED
+        )
+    )
+
+    private val backButtonEvent = TrackEvent.Icon(
+        text = context.getEnglishString(R.string.system_backButton),
+        params = RequiredParameters(
+            taxonomyLevel2 = TaxonomyLevel2.APP_SYSTEM,
+            taxonomyLevel3 = TaxonomyLevel3.UNDEFINED
+        )
+    )
 }
