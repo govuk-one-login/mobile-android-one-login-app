79a099a048399676ea7b04003feeff9233156429
Bianca Mihaila <bianca.mihaila@digital.cabinet-office.gov.uk>
2025-03-11 11:28:55 +0000

    chore: Update `runInstrumentationChecks` script
    - run managed devices/ Android tests only on `app` in
    `build` and `production` because `core` and `features` do
    not have any Android tests that cannot be run with Robolectric
    as this modules need only unit testing
    - move and fix `TextWithLinkTest` to run as unit test
    
    Resolves: DCMAW-10229

--- core/src/test/java/uk/gov/onelogin/core/ui/components/TextWithLinkTest.kt
+++ core/src/test/java/uk/gov/onelogin/core/ui/components/TextWithLinkTest.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.test.click
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performTouchInput
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
@@ -13,17 +14,16 @@ import androidx.compose.ui.text.withStyle
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
 import org.junit.runner.RunWith
-import uk.gov.onelogin.core.TestCase
+import uk.gov.onelogin.core.FragmentActivityTestCase
 
 @RunWith(AndroidJUnit4::class)
-class TextWithLinkTest : TestCase() {
+class TextWithLinkTest : FragmentActivityTestCase() {
     @Test
     fun testOnlyLinkTextClickOnLink() {
         var clickIndex = -1
         composeTestRule.setContent {
             TextWithLink(
                 linkText = AnnotatedString("Click Me"),
-                modifier = Modifier.testTag("ClickableText"),
                 onClick = { index ->
                     clickIndex = index
                 }
@@ -31,13 +31,13 @@ class TextWithLinkTest : TestCase() {
         }
 
         // Perform a click in the middle of the text
-        composeTestRule.onNodeWithTag("ClickableText", useUnmergedTree = true)
+        composeTestRule.onNodeWithText("Click Me", useUnmergedTree = true, substring = true)
             .performTouchInput {
                 click(center)
             }
 
         // Assert that the click was received in the correct range
-        assert(clickIndex in 4..7) // "Click Me" - click should be somewhere within "Me"
+        assert(clickIndex in 4..8) // "Click Me" - click should be somewhere within "Me"
     }
 
     @Test
@@ -106,7 +106,6 @@ class TextWithLinkTest : TestCase() {
             TextWithLink(
                 text = "Normal Text",
                 linkText = annotatedString,
-                modifier = Modifier.testTag("ClickableText"),
                 onClick = {
                     clicked = true
                 }
@@ -114,9 +113,9 @@ class TextWithLinkTest : TestCase() {
         }
 
         // Perform a click in the middle of the link text
-        composeTestRule.onNodeWithTag("ClickableText", useUnmergedTree = true)
+        composeTestRule.onNodeWithText("Click Me", useUnmergedTree = true, substring = true)
             .performTouchInput {
-                click(bottomRight.copy(x = bottomRight.x - 10f)) // Click in the "Click me part
+                click() // Click in the "Click me part
             }
 
         // Assert that the click was handled
