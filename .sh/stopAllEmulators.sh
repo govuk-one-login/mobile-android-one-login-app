ba3bb5bf0bef9bdff1c606993db72d98b814e403
Alex Bradbury <alex.bradbury@digital.cabinet-office.gov.uk>
2025-03-18 23:44:56 +0000

    fix: sort managed devices
    Resolves: DCMAW-8868

--- .sh/stopAllEmulators.sh
+++ .sh/stopAllEmulators.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env sh
 
-# Obtains all devices that begin with 'emulator'
+ Obtains all devices that begin with 'emulator'
 for DEVICE in $(adb devices | grep emulator | cut -f1)
 do
   # Close the device; ignoring standard output
   adb -s "$DEVICE" emu kill > /dev/null 2>&1
 done
-./gradlew cleanManagedDevices
