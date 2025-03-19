03553869945079c8ab8d580523c5489125089749
Alex Bradbury <alex.bradbury@digital.cabinet-office.gov.uk>
2025-03-19 08:26:09 +0000

    fix: fix script commented line
    Resolves: DCMAW-8868

--- .sh/stopAllEmulators.sh
+++ .sh/stopAllEmulators.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
- Obtains all devices that begin with 'emulator'
+# Obtains all devices that begin with 'emulator'
 for DEVICE in $(adb devices | grep emulator | cut -f1)
 do
   # Close the device; ignoring standard output
