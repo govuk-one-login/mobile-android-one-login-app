6b7fc1352e9ae9d99edc4116a53a2879c8938b29
Chris Donnelly <205921253+chrisdondigital@users.noreply.github.com>
2025-05-01 11:15:21 +0100

    - Remove redundant onNewIntent method call from MainActivity
    Resolves: DCMAW-8902

--- app/src/main/java/uk/gov/onelogin/MainActivity.kt
+++ app/src/main/java/uk/gov/onelogin/MainActivity.kt
@@ -1,6 +1,5 @@
 package uk.gov.onelogin
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -36,11 +35,5 @@ class MainActivity : AppCompatActivity() {
 
             OneLoginApp(navController = navController)
         }
-    }
-
-    override fun onNewIntent(intent: Intent) {
-        super.onNewIntent(intent)
-
-        viewModel.handleIntent(intent)
     }
 }
