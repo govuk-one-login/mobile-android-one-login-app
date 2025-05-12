d80fb9ef529c39efcf0865992edd16a783570579
Chris Donnelly <205921253+chrisdondigital@users.noreply.github.com>
2025-05-12 16:02:02 +0100

    - Add night mode test
    Resolves: DCMAW-11704

--- core/src/test/java/uk/gov/onelogin/core/ui/pages/EdgeToEdgePageTest.kt
+++ core/src/test/java/uk/gov/onelogin/core/ui/pages/EdgeToEdgePageTest.kt
@@ -1,5 +1,9 @@
 package uk.gov.onelogin.core.ui.pages
 
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.displayCutout
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.onNodeWithText
@@ -27,6 +31,29 @@ class EdgeToEdgePageTest : FragmentActivityTestCase() {
         val expectedText = "Test"
         composeTestRule.setContent {
             EdgeToEdgePage(modifier = Modifier) {
+                Text(expectedText)
+            }
+        }
+
+        composeTestRule.onNodeWithText(expectedText).assertExists()
+    }
+
+    @Test
+    fun testPageWithModifierWindowInsets() {
+        val expectedText = "Test"
+        composeTestRule.setContent {
+            EdgeToEdgePage(modifier = Modifier.windowInsetsPadding(WindowInsets.displayCutout)) {
+                Text(expectedText)
+            }
+        }
+
+        composeTestRule.onNodeWithText(expectedText).assertExists()
+    }
+
+    fun testPageWithModifierNavigationBars() {
+        val expectedText = "Test"
+        composeTestRule.setContent {
+            EdgeToEdgePage(modifier = Modifier.navigationBarsPadding()) {
                 Text(expectedText)
             }
         }
