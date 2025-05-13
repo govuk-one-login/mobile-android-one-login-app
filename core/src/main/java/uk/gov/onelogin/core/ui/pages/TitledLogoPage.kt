a7b3ef1514e69862598553614319c130e64de8a0
Chris Donnelly <205921253+chrisdondigital@users.noreply.github.com>
2025-05-13 12:16:00 +0100

    - Add custom app bar class to allow image to be displayed without size scaling
    Resolves: DCMAW-11704

--- core/src/main/java/uk/gov/onelogin/core/ui/pages/TitledLogoPage.kt
+++ core/src/main/java/uk/gov/onelogin/core/ui/pages/TitledLogoPage.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
@@ -23,8 +25,10 @@ import androidx.compose.ui.semantics.heading
 import androidx.compose.ui.semantics.semantics
 import uk.gov.android.onelogin.core.R
 import uk.gov.android.ui.theme.spacingDouble
-import uk.gov.ui.components.appbar.GdsTopAppBar
+import uk.gov.onelogin.core.ui.components.FlexibleTopBar
+import uk.gov.onelogin.core.ui.components.FlexibleTopBarColors
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TitledLogoPage(
     @DrawableRes logo: Int,
@@ -34,10 +38,14 @@ fun TitledLogoPage(
 
     Scaffold(
         topBar = {
-            GdsTopAppBar(
-                title = {
+            FlexibleTopBar(
+                scrollBehavior = scrollBehavior,
+                content = {
                     Box(
-                        modifier = Modifier.padding(vertical = spacingDouble),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = spacingDouble)
+                            .statusBarsPadding(),
                         contentAlignment = Alignment.Center
                     ) {
                         Image(
@@ -45,18 +53,17 @@ fun TitledLogoPage(
                             contentDescription = stringResource(
                                 R.string.one_login_image_content_desc
                             ),
-                            modifier = Modifier.semantics { heading() }
+                            modifier = Modifier
+                                .semantics { heading() }
                         )
                     }
                 },
-                colors = {
-                    TopAppBarDefaults.centerAlignedTopAppBarColors(
-                        containerColor = getAppBarColor(),
-                        scrolledContainerColor = getAppBarColor()
-                    )
-                },
-                scrollBehavior = scrollBehavior
-            ).generate()
+                colors =
+                FlexibleTopBarColors(
+                    containerColor = getAppBarColor(),
+                    scrolledContainerColor = getAppBarColor()
+                )
+            )
         },
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
     ) { paddingValues ->
