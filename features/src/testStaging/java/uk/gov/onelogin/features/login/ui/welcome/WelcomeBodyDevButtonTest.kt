36311ea6ea3af549ea92673c4fd12a0d9357276d
Bianca Mihaila <bianca.mihaila@digital.cabinet-office.gov.uk>
2025-05-15 12:13:07 +0100

    test: Add missing tests for staging
    Resolves: DCMAW-12966

---
+++ features/src/testStaging/java/uk/gov/onelogin/features/login/ui/welcome/WelcomeBodyDevButtonTest.kt
@@ -0,0 +1,33 @@
+package uk.gov.onelogin.features.login.ui.welcome
+
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.performClick
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import uk.gov.android.onelogin.core.R
+import uk.gov.onelogin.features.FragmentActivityTestCase
+import uk.gov.onelogin.features.login.ui.signin.welcome.WelcomeBody
+
+@RunWith(AndroidJUnit4::class)
+class WelcomeBodyDevButtonTest : FragmentActivityTestCase() {
+    private val devButton = hasText(resources.getString(R.string.app_developer_button))
+
+    @Test
+    fun onAccessDevMenu() {
+        // Given the WelcomeBody Composable
+        var actual = false
+        composeTestRule.setContent {
+            WelcomeBody(
+                onSignIn = {},
+                openDevMenu = { actual = true }
+            )
+        }
+
+        // When clicking the icon
+        composeTestRule.onNode(devButton, useUnmergedTree = true).performClick()
+        // Then openDevMenu() is called and the variable is true
+        assertEquals(true, actual)
+    }
+}
