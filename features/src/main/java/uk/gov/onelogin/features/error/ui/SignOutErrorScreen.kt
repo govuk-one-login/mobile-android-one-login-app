3ff727315fd402d75d4d0ee1aec6f6b39a61478b
alex-bradbury-gds <alex.bradbury@digital.cabinet-office.gov.uk>
2025-05-20 19:31:43 +0100

    feat: move error screens onto M3 `ErrorScreen`
    Resolves: DCMAW-13448

--- features/src/main/java/uk/gov/onelogin/features/error/ui/SignOutErrorScreen.kt
+++ features/src/main/java/uk/gov/onelogin/features/error/ui/SignOutErrorScreen.kt
@@ -1,17 +1,13 @@
 package uk.gov.onelogin.features.error.ui
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import kotlinx.collections.immutable.persistentListOf
 import uk.gov.android.onelogin.core.R
-import uk.gov.android.ui.components.HeadingSize
-import uk.gov.android.ui.components.buttons.ButtonParameters
-import uk.gov.android.ui.components.buttons.ButtonType
-import uk.gov.android.ui.components.content.ContentParameters
-import uk.gov.android.ui.components.content.GdsContentText
-import uk.gov.android.ui.components.images.icon.IconParameters
-import uk.gov.android.ui.components.information.InformationParameters
-import uk.gov.android.ui.pages.errors.ErrorPage
-import uk.gov.android.ui.pages.errors.ErrorPageParameters
+import uk.gov.android.ui.patterns.centrealignedscreen.CentreAlignedScreenBodyContent
+import uk.gov.android.ui.patterns.centrealignedscreen.CentreAlignedScreenButton
+import uk.gov.android.ui.patterns.errorscreen.ErrorScreen
+import uk.gov.android.ui.patterns.errorscreen.ErrorScreenIcon
 import uk.gov.android.ui.theme.m3.GdsTheme
 import uk.gov.android.ui.theme.meta.ExcludeFromJacocoGeneratedReport
 import uk.gov.android.ui.theme.meta.ScreenPreview
@@ -21,31 +17,18 @@ import uk.gov.onelogin.core.ui.pages.EdgeToEdgePage
 fun SignOutErrorScreen(onExitAppClicked: () -> Unit) {
     GdsTheme {
         EdgeToEdgePage { _ ->
-            ErrorPage(
-                parameters = ErrorPageParameters(
-                    primaryButtonParameters = ButtonParameters(
-                        buttonType = ButtonType.PRIMARY(),
-                        onClick = onExitAppClicked,
-                        text = R.string.app_exitButton
-                    ),
-                    informationParameters = InformationParameters(
-                        contentParameters = ContentParameters(
-                            resource = listOf(
-                                GdsContentText.GdsContentTextString(
-                                    subTitle = R.string.app_signOutErrorTitle,
-                                    text =
-                                    intArrayOf(
-                                        R.string.app_signOutErrorBody
-                                    )
-                                )
-                            ),
-                            headingSize = HeadingSize.H1()
-                        ),
-                        iconParameters = IconParameters(
-                            foreGroundColor = Color.Unspecified,
-                            image = uk.gov.android.ui.components.R.drawable.ic_error
-                        )
+            ErrorScreen(
+                icon = ErrorScreenIcon.ErrorIcon,
+                title = stringResource(R.string.app_signOutErrorTitle),
+                body = persistentListOf(
+                    CentreAlignedScreenBodyContent.Text(
+                        bodyText = stringResource(R.string.app_signOutErrorBody)
                     )
+                ),
+                primaryButton =
+                CentreAlignedScreenButton(
+                    text = stringResource(R.string.app_exitButton),
+                    onClick = onExitAppClicked
                 )
             )
         }
