f08871666f35f07896edf80120ae5e1ffa7534a4
Bianca Mihaila <bianca.mihaila@digital.cabinet-office.gov.uk>
2025-10-29 14:08:56 +0000

    fix: Fix linting issue

--- features/src/test/java/uk/gov/onelogin/features/temptestpackage/TestTempUnitTest.kt
+++ features/src/test/java/uk/gov/onelogin/features/temptestpackage/TestTempUnitTest.kt
@@ -1,10 +1,11 @@
 package uk.gov.onelogin.features.temptestpackage
 
-import org.junit.Test
 import kotlin.test.assertEquals
+import org.junit.Test
 
 class TestTempUnitTest {
     val sut = TestTemp()
+
     @Test
     fun test() {
         assertEquals(2, sut.calculate(1, 1))
