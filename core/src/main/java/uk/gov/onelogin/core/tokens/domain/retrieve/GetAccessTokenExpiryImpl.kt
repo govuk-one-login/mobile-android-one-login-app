245c23897ad42094bba64b03ee2dcfb0df2fb410
Bianca Mihaila <bianca.mihaila@digital.cabinet-office.gov.uk>
2026-01-26 14:57:04 +0000

    refactor: Adress PR comments to encapsulate functionality better

--- core/src/main/java/uk/gov/onelogin/core/tokens/domain/retrieve/GetAccessTokenExpiryImpl.kt
+++ core/src/main/java/uk/gov/onelogin/core/tokens/domain/retrieve/GetAccessTokenExpiryImpl.kt
@@ -12,13 +12,6 @@ class GetAccessTokenExpiryImpl @Inject constructor(
     private val context: Context,
     private val getFromOpenSecureStore: GetFromOpenSecureStore
 ) : GetTokenExpiry {
-    // The SharedPrefs will be removed in a period to be determined to allow for migration for all users and avoid
-    // creating a breaking change that forces the user to be treated as a new user.
-    private val sharedPrefs = context.getSharedPreferences(
-        TOKEN_SHARED_PREFS,
-        Context.MODE_PRIVATE
-    )
-
     override suspend fun invoke(): Long? {
         try {
             val expiryTimestamp = getFromOpenSecureStore(ACCESS_TOKEN_EXPIRY_KEY)
@@ -34,6 +27,12 @@ class GetAccessTokenExpiryImpl @Inject constructor(
     }
 
     private fun checkAndReturnSharedPrefsExpiry(): Long? {
+        // The SharedPrefs will be removed in a period to be determined to allow for migration for all users and avoid
+        // creating a breaking change that forces the user to be treated as a new user.
+        val sharedPrefs = context.getSharedPreferences(
+            TOKEN_SHARED_PREFS,
+            Context.MODE_PRIVATE
+        )
         // The SharedPrefs will be removed in a period to be determined to allow for migration for all users and avoid
         // creating a breaking change that forces the user to be treated as a new user.
         val expiryTimestampSharedPrefs = sharedPrefs.getLong(ACCESS_TOKEN_EXPIRY_KEY, 0)
