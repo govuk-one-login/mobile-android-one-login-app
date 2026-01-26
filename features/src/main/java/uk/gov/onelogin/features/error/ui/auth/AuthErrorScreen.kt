bd81e80898d3489e60ba04cfef6413c922cd945c
Elijah Hezekiah <elijah.hezekiah@digital.cabinet-office.gov.uk>
2026-01-26 18:01:21 +0000

    Fix: Updated AuthErrorBody from V1 to use V2
    - ErrorScreen Composable now uses extracted Modifier in individual elements.

--- features/src/main/java/uk/gov/onelogin/features/error/ui/auth/AuthErrorScreen.kt
+++ features/src/main/java/uk/gov/onelogin/features/error/ui/auth/AuthErrorScreen.kt
@@ -1,15 +1,26 @@
 package uk.gov.onelogin.features.error.ui.auth
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.collections.immutable.persistentListOf
 import uk.gov.android.onelogin.core.R
-import uk.gov.android.ui.patterns.centrealignedscreen.CentreAlignedScreenBodyContent
-import uk.gov.android.ui.patterns.centrealignedscreen.CentreAlignedScreenButton
-import uk.gov.android.ui.patterns.errorscreen.ErrorScreen
-import uk.gov.android.ui.patterns.errorscreen.ErrorScreenIcon
+import uk.gov.android.ui.componentsv2.button.ButtonTypeV2
+import uk.gov.android.ui.componentsv2.button.GdsButton
+import uk.gov.android.ui.componentsv2.heading.GdsHeading
+import uk.gov.android.ui.componentsv2.heading.GdsHeadingAlignment
+import uk.gov.android.ui.componentsv2.images.GdsIcon
+import uk.gov.android.ui.patterns.errorscreen.v2.ErrorScreen
+import uk.gov.android.ui.theme.largePadding
 import uk.gov.android.ui.theme.m3.GdsTheme
+import uk.gov.android.ui.theme.util.UnstableDesignSystemAPI
 import uk.gov.onelogin.core.ui.meta.ExcludeFromJacocoGeneratedReport
 import uk.gov.onelogin.core.ui.meta.ScreenPreview
 import uk.gov.onelogin.core.ui.pages.EdgeToEdgePage
@@ -25,38 +36,59 @@ fun AuthErrorScreen(viewModel: AuthErrorViewModel = hiltViewModel()) {
     }
 }
 
+@OptIn(UnstableDesignSystemAPI::class)
 @Composable
 private fun AuthErrorBody(goToSignIn: () -> Unit = {}) {
     val bodyContent =
         persistentListOf(
-            CentreAlignedScreenBodyContent.Text(
-                bodyText = stringResource(R.string.app_dataDeletedBody1),
-            ),
-            CentreAlignedScreenBodyContent.Text(
-                bodyText = stringResource(R.string.app_dataDeletedBody2),
-            ),
-            CentreAlignedScreenBodyContent.Text(
-                bodyText = stringResource(R.string.app_dataDeletedBody3),
-            ),
+            stringResource(R.string.app_dataDeletedBody1),
+            stringResource(R.string.app_dataDeletedBody2),
+            stringResource(R.string.app_dataDeletedBody3)
         )
 
     ErrorScreen(
-        icon = ErrorScreenIcon.ErrorIcon,
-        title = stringResource(R.string.app_dataDeletedErrorTitle),
-        body = bodyContent,
-        primaryButton =
-            CentreAlignedScreenButton(
-                text = stringResource(R.string.app_dataDeletedButton),
+        icon = {
+            GdsIcon(
+                image = ImageVector.vectorResource(uk.gov.android.ui.patterns.R.drawable.ic_warning_error),
+                contentDescription = stringResource(uk.gov.android.ui.componentsv2.R.string.warning),
+                modifier = itemModifier().padding(bottom = largePadding)
+            )
+        },
+        title = {
+            GdsHeading(
+                text = stringResource(R.string.app_dataDeletedErrorTitle),
+                modifier = itemModifier().padding(horizontal = largePadding),
+                textAlign = GdsHeadingAlignment.CenterAligned,
+            )
+        },
+        body = {
+            items(bodyContent.size) { index ->
+                Text(
+                    text = bodyContent[index],
+                    textAlign = TextAlign.Center,
+                    modifier = itemModifier().padding(horizontal = largePadding)
+                )
+            }
+        },
+        primaryButton = {
+            val text = stringResource(R.string.app_dataDeletedButton)
+            GdsButton(
+                text = text,
+                buttonType = ButtonTypeV2.Primary(),
                 onClick = goToSignIn,
-            ),
+                modifier = Modifier.fillMaxWidth()
+            )
+        },
     )
 }
+
+private fun itemModifier(): Modifier = Modifier.fillMaxWidth()
 
 @ExcludeFromJacocoGeneratedReport
 @ScreenPreview
 @Composable
 internal fun AuthErrorScreenPreview() {
     GdsTheme {
-        AuthErrorBody()
+        AuthErrorBody ()
     }
 }
