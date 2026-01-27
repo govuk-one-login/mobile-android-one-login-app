c22c886a84f0a6c6efd8a9e35dc72f849dbef633
Elijah Hezekiah <elijah.hezekiah@digital.cabinet-office.gov.uk>
2026-01-27 12:53:29 +0000

    Fix: Updated GenericErrorScreen.kt from V1 to use V2

--- features/src/main/java/uk/gov/onelogin/features/error/ui/generic/GenericErrorScreen.kt
+++ features/src/main/java/uk/gov/onelogin/features/error/ui/generic/GenericErrorScreen.kt
@@ -1,17 +1,28 @@
 package uk.gov.onelogin.features.error.ui.generic
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.collections.immutable.persistentListOf
 import uk.gov.android.onelogin.core.R
-import uk.gov.android.ui.patterns.centrealignedscreen.CentreAlignedScreenBodyContent
-import uk.gov.android.ui.patterns.centrealignedscreen.CentreAlignedScreenButton
-import uk.gov.android.ui.patterns.errorscreen.ErrorScreen
-import uk.gov.android.ui.patterns.errorscreen.ErrorScreenIcon
+import uk.gov.android.ui.componentsv2.button.ButtonTypeV2
+import uk.gov.android.ui.componentsv2.button.GdsButton
+import uk.gov.android.ui.componentsv2.heading.GdsHeading
+import uk.gov.android.ui.componentsv2.heading.GdsHeadingAlignment
+import uk.gov.android.ui.componentsv2.images.GdsIcon
+import uk.gov.android.ui.patterns.errorscreen.v2.ErrorScreen
+import uk.gov.android.ui.theme.largePadding
 import uk.gov.android.ui.theme.m3.GdsTheme
+import uk.gov.android.ui.theme.util.UnstableDesignSystemAPI
 import uk.gov.onelogin.core.ui.meta.ExcludeFromJacocoGeneratedReport
 import uk.gov.onelogin.core.ui.meta.ScreenPreview
 import uk.gov.onelogin.core.ui.pages.EdgeToEdgePage
@@ -36,30 +47,58 @@ fun GenericErrorScreen(
     }
 }
 
+@OptIn(UnstableDesignSystemAPI::class)
 @Composable
 private fun GenericErrorBody(primaryOnClick: () -> Unit = {}) {
+
+    val bodyContent =
+        persistentListOf(
+            stringResource(R.string.app_genericErrorPageBody),
+        )
+
     ErrorScreen(
-        icon = ErrorScreenIcon.ErrorIcon,
-        title = stringResource(R.string.app_genericErrorPage),
-        body =
-            persistentListOf(
-                CentreAlignedScreenBodyContent.Text(
-                    bodyText = stringResource(R.string.app_genericErrorPageBody),
-                ),
-            ),
-        primaryButton =
-            CentreAlignedScreenButton(
-                text = stringResource(R.string.app_genericErrorPageButton),
+        icon = {
+            GdsIcon(
+                image = ImageVector.vectorResource(uk.gov.android.ui.patterns.R.drawable.ic_warning_error),
+                contentDescription = stringResource(uk.gov.android.ui.componentsv2.R.string.warning),
+                modifier = genericErrorScreenModifier().padding(bottom = largePadding)
+            )
+        },
+        title = {
+            GdsHeading(
+                text = stringResource(R.string.app_genericErrorPage),
+                modifier = genericErrorScreenModifier().padding(horizontal = largePadding),
+                textAlign = GdsHeadingAlignment.CenterAligned,
+            )
+        },
+        body = {
+            items(bodyContent.size) { index ->
+                Text(
+                    text = bodyContent[index],
+                    textAlign = TextAlign.Center,
+                    modifier = genericErrorScreenModifier().padding(horizontal = largePadding)
+                )
+            }
+        },
+        primaryButton = {
+            val text = stringResource(R.string.app_genericErrorPageButton)
+            GdsButton(
+                text = text,
+                buttonType = ButtonTypeV2.Primary(),
                 onClick = primaryOnClick,
-            ),
+                modifier = Modifier.fillMaxWidth()
+            )
+        },
     )
 }
+
+private fun genericErrorScreenModifier () : Modifier = Modifier.fillMaxWidth()
 
 @ExcludeFromJacocoGeneratedReport
 @ScreenPreview
 @Composable
 internal fun GenericErrorPreview() {
     GdsTheme {
-        GenericErrorBody()
+        GenericErrorBody ()
     }
 }
