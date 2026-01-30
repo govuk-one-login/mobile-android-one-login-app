273e551e14076665d406711b4a94e72072aacb7a
Elijah Hezekiah <elijah.hezekiah@digital.cabinet-office.gov.uk>
2026-01-30 13:27:36 +0000

    Fix: update SignInErrorUnrecoverableScreen from V1 to V2

--- features/src/main/java/uk/gov/onelogin/features/error/ui/generic/GenericErrorScreen.kt
+++ features/src/main/java/uk/gov/onelogin/features/error/ui/generic/GenericErrorScreen.kt
@@ -3,7 +3,7 @@ package uk.gov.onelogin.features.error.ui.generic
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
--- features/src/main/java/uk/gov/onelogin/features/error/ui/signin/SignInErrorUnrecoverableScreen.kt
+++ features/src/main/java/uk/gov/onelogin/features/error/ui/signin/SignInErrorUnrecoverableScreen.kt
@@ -3,18 +3,27 @@ package uk.gov.onelogin.features.error.ui.signin
 import android.app.Activity
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.LocalActivity
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.collections.immutable.persistentListOf
 import uk.gov.android.onelogin.core.R
-import uk.gov.android.ui.patterns.centrealignedscreen.CentreAlignedScreenBodyContent
-import uk.gov.android.ui.patterns.errorscreen.ErrorScreen
-import uk.gov.android.ui.patterns.errorscreen.ErrorScreenIcon
+import uk.gov.android.ui.componentsv2.heading.GdsHeading
+import uk.gov.android.ui.componentsv2.heading.GdsHeadingAlignment
+import uk.gov.android.ui.componentsv2.images.GdsIcon
+import uk.gov.android.ui.theme.largePadding
 import uk.gov.android.ui.theme.m3.GdsTheme
 import uk.gov.android.ui.theme.meta.ExcludeFromJacocoGeneratedReport
 import uk.gov.android.ui.theme.meta.ScreenPreview
+import uk.gov.android.ui.theme.util.UnstableDesignSystemAPI
 import uk.gov.onelogin.core.ui.pages.EdgeToEdgePage
 
 @Composable
@@ -35,19 +44,42 @@ fun SignInErrorUnrecoverableScreen(
     }
 }
 
+@OptIn(UnstableDesignSystemAPI::class)
 @Composable
 private fun SignInErrorBody() {
-    ErrorScreen(
-        icon = ErrorScreenIcon.ErrorIcon,
-        title = stringResource(R.string.app_signInErrorTitle),
-        body =
-            persistentListOf(
-                CentreAlignedScreenBodyContent.Text(
-                    bodyText = stringResource(R.string.app_genericErrorPageBody),
-                ),
-            ),
+    val bodyContent =
+        persistentListOf(
+            stringResource(R.string.app_genericErrorPageBody),
+        )
+
+    uk.gov.android.ui.patterns.errorscreen.v2.ErrorScreen(
+        icon = {
+            GdsIcon(
+                image = ImageVector.vectorResource(uk.gov.android.ui.patterns.R.drawable.ic_warning_error),
+                contentDescription = stringResource(uk.gov.android.ui.componentsv2.R.string.warning),
+                modifier = signInErrorBodyModifier().padding(bottom = largePadding)
+            )
+        },
+        title = {
+            GdsHeading(
+                text = stringResource(R.string.app_signInErrorTitle),
+                modifier = signInErrorBodyModifier().padding(horizontal = largePadding),
+                textAlign = GdsHeadingAlignment.CenterAligned,
+            )
+        },
+        body = {
+            items(bodyContent.size) { index ->
+                Text(
+                    text = bodyContent[index],
+                    textAlign = TextAlign.Center,
+                    modifier = signInErrorBodyModifier().padding(horizontal = largePadding)
+                )
+            }
+        }
     )
 }
+
+private fun signInErrorBodyModifier(): Modifier = Modifier.fillMaxWidth()
 
 @ExcludeFromJacocoGeneratedReport
 @ScreenPreview
