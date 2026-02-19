b426caacef7168b4f54761b97890f729acf9f6b1
alex-bradbury-gds <alex.bradbury@digital.cabinet-office.gov.uk>
2026-02-19 19:31:37 +0000

    refactor: move intent to common val
    Resolves: DCMAW-18181

--- app/src/main/java/uk/gov/onelogin/DeeplinkActivity.kt
+++ app/src/main/java/uk/gov/onelogin/DeeplinkActivity.kt
@@ -9,28 +9,23 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class DeeplinkActivity : AppCompatActivity() {
     private val viewModel: DeeplinkActivityViewModel by viewModels()
+    private val mainActivityIntent =
+        Intent(this, MainActivity::class.java)
+            .apply {
+                flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+            }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         viewModel.handleIntent(intent)
-        val intent =
-            Intent(this, MainActivity::class.java)
-                .apply {
-                    flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
-                }
-        startActivity(intent)
+        startActivity(mainActivityIntent)
     }
 
     override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
         setIntent(intent)
         viewModel.handleIntent(intent)
-        val mainIntent =
-            Intent(this, MainActivity::class.java)
-                .apply {
-                    flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
-                }
-        startActivity(mainIntent)
+        startActivity(mainActivityIntent)
     }
 }
