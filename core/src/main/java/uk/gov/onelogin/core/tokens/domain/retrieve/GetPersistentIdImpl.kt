168fa1d43e083a007d530377e61da1897d288d5c
Bianca Mihaila <bianca.mihaila@digital.cabinet-office.gov.uk>
2026-03-16 14:59:32 +0000

    feat: Add fucntionality to log wallet deletion when unexpected

--- core/src/main/java/uk/gov/onelogin/core/tokens/domain/retrieve/GetPersistentIdImpl.kt
+++ core/src/main/java/uk/gov/onelogin/core/tokens/domain/retrieve/GetPersistentIdImpl.kt
@@ -1,5 +1,8 @@
 package uk.gov.onelogin.core.tokens.domain.retrieve
 
+import uk.gov.android.wallet.sdk.WalletSdk
+import uk.gov.logging.api.v2.Logger
+import uk.gov.logging.api.v2.errorKeys.ErrorKeys
 import uk.gov.onelogin.core.tokens.utils.AuthTokenStoreKeys
 import javax.inject.Inject
 
@@ -7,9 +10,32 @@ class GetPersistentIdImpl
     @Inject
     constructor(
         val getFromOpenSecureStore: GetFromOpenSecureStore,
+        private val walletSdk: WalletSdk,
+        private val logger: Logger,
     ) : GetPersistentId {
-        override suspend fun invoke(): String? =
-            getFromOpenSecureStore(AuthTokenStoreKeys.PERSISTENT_ID_KEY)?.get(
-                AuthTokenStoreKeys.PERSISTENT_ID_KEY,
-            )
+        override suspend fun invoke(): String?
+            {
+                val id = getFromOpenSecureStore(AuthTokenStoreKeys.PERSISTENT_ID_KEY)?.get(
+                    AuthTokenStoreKeys.PERSISTENT_ID_KEY,
+                )
+                try {
+                    if (id.isNullOrEmpty() && !walletSdk.isEmpty()) {
+                        val exp = Exception("Wallet is not empty")
+                        logger.error(
+                            this.javaClass.simpleName,
+                            exp.message ?: "error",
+                            exp,
+                            ErrorKeys.StringKey("reason", "secure wallet data deleted")
+                        )
+                    }
+                } catch (e: Throwable) {
+                    logger.error(
+                        this.javaClass.simpleName,
+                        e.message ?: "error",
+                        e,
+                        ErrorKeys.StringKey("reason", "secure wallet data deleted")
+                    )
+                }
+                return id
+            }
     }
