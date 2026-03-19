f0a7b9f8e9e8a05ee835bf54117150a4730e61b5
Chris Donnelly <205921253+chrisdondigital@users.noreply.github.com>
2026-03-19 15:11:55 +0000

    feat: Use open secure store to persist token expiry
    Resolves: DCMAW-18955

--- core/src/main/java/uk/gov/onelogin/core/tokens/domain/remove/RemoveRefreshTokenAndExpiryImpl.kt
+++ core/src/main/java/uk/gov/onelogin/core/tokens/domain/remove/RemoveRefreshTokenAndExpiryImpl.kt
@@ -1,36 +1,24 @@
 package uk.gov.onelogin.core.tokens.domain.remove
 
-import android.content.Context
-import androidx.core.content.edit
-import dagger.hilt.android.qualifiers.ApplicationContext
 import uk.gov.android.securestore.SecureStoreAsyncV2
 import uk.gov.android.securestore.error.SecureStorageErrorV2
 import uk.gov.logging.api.Logger
 import uk.gov.onelogin.core.tokens.utils.AuthTokenStoreKeys
-import uk.gov.onelogin.core.tokens.utils.AuthTokenStoreKeys.REFRESH_TOKEN_EXPIRY_KEY
-import uk.gov.onelogin.core.tokens.utils.AuthTokenStoreKeys.TOKEN_SHARED_PREFS
 import javax.inject.Inject
 import javax.inject.Named
 
 class RemoveRefreshTokenAndExpiryImpl
     @Inject
     constructor(
-        @ApplicationContext
-        context: Context,
         @param:Named("Token")
         private val tokenSecureStore: SecureStoreAsyncV2,
+        @param:Named("Open")
+        private val openSecureStore: SecureStoreAsyncV2,
         private val logger: Logger,
     ) : RemoveRefreshTokenAndExpiry {
-        private val sharedPrefs =
-            context.getSharedPreferences(TOKEN_SHARED_PREFS, Context.MODE_PRIVATE)
-
         override suspend fun remove() {
-            sharedPrefs.edit {
-                remove(REFRESH_TOKEN_EXPIRY_KEY)
-                commit()
-            }
-
             try {
+                openSecureStore.delete(AuthTokenStoreKeys.REFRESH_TOKEN_EXPIRY_KEY)
                 tokenSecureStore.delete(AuthTokenStoreKeys.REFRESH_TOKEN_KEY)
                 Result.success(Unit)
             } catch (e: SecureStorageErrorV2) {
--- core/src/test/java/uk/gov/onelogin/core/tokens/domain/remove/RemoveRefreshTokenAndExpiryTest.kt
+++ core/src/test/java/uk/gov/onelogin/core/tokens/domain/remove/RemoveRefreshTokenAndExpiryTest.kt
@@ -1,11 +1,8 @@
 package uk.gov.onelogin.core.tokens.domain.remove
 
-import android.content.Context
-import android.content.SharedPreferences
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.eq
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
@@ -18,24 +15,14 @@ import kotlin.test.assertTrue
 class RemoveRefreshTokenAndExpiryTest {
     private lateinit var sut: RemoveRefreshTokenAndExpiryImpl
     private val mockTokenSecureStore: SecureStoreAsyncV2 = mock()
+    private val mockOpenSecureStore: SecureStoreAsyncV2 = mock()
     private val mockLogger = SystemLogger()
-    private val mockContext: Context = mock()
-    private val mockSharedPreferences: SharedPreferences = mock()
-    private val mockEditor: SharedPreferences.Editor = mock()
 
     @BeforeEach
     fun setup() {
-        whenever(
-            mockContext.getSharedPreferences(
-                eq(AuthTokenStoreKeys.TOKEN_SHARED_PREFS),
-                eq(Context.MODE_PRIVATE),
-            ),
-        ).thenReturn(mockSharedPreferences)
-        whenever(mockSharedPreferences.edit()).thenReturn(mockEditor)
-
         sut =
             RemoveRefreshTokenAndExpiryImpl(
-                context = mockContext,
+                openSecureStore = mockOpenSecureStore,
                 tokenSecureStore = mockTokenSecureStore,
                 logger = mockLogger
             )
@@ -46,17 +33,27 @@ class RemoveRefreshTokenAndExpiryTest {
         runTest {
             sut.remove()
 
-            verify(mockEditor).remove(AuthTokenStoreKeys.REFRESH_TOKEN_EXPIRY_KEY)
-            verify(mockEditor).commit()
+            verify(mockOpenSecureStore).delete(AuthTokenStoreKeys.REFRESH_TOKEN_EXPIRY_KEY)
             verify(mockTokenSecureStore).delete(AuthTokenStoreKeys.REFRESH_TOKEN_KEY)
         }
 
     @Test
-    fun testRemoveSecureStoreFailure() =
+    fun testRemoveTokenSecureStoreFailure() =
         runTest {
             val message = "Error message"
             val exception = SecureStorageErrorV2(Exception(message))
             whenever(mockTokenSecureStore.delete(AuthTokenStoreKeys.REFRESH_TOKEN_KEY)).thenThrow(exception)
+            sut.remove()
+
+            assertTrue(mockLogger.contains("java.lang.Exception: $message"))
+        }
+
+    @Test
+    fun testRemoveOpenSecureStoreFailure() =
+        runTest {
+            val message = "Error message"
+            val exception = SecureStorageErrorV2(Exception(message))
+            whenever(mockOpenSecureStore.delete(AuthTokenStoreKeys.REFRESH_TOKEN_EXPIRY_KEY)).thenThrow(exception)
             sut.remove()
 
             assertTrue(mockLogger.contains("java.lang.Exception: $message"))
