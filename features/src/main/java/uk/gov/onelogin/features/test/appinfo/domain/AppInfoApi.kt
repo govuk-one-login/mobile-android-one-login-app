07b9ce4cd3ecd96adf0f4164295e9feeee9e6648
Bianca Mihaila <bianca.mihaila@digital.cabinet-office.gov.uk>
2026-04-20 13:43:56 +0100

    ci: Split on-pull-request in differnet jobs within a worklfow
    - create jobs for each type of tests to be run (unit, instrumentation, style, component and snapshot)
    - run those checks in parallel
    - run sonar analysis ina a separate job dependnent on successfull run of all check jobs
    - update mobile-pipelines to enable use of the new upload coverage reports using github upload artifact
    - create restore coverage reports action that downaload the reports previously uploaded, unzipa and sorts
    those in teh correct format for the sonar analysis
    
    Resolves: DCMAW-19621

--- features/src/main/java/uk/gov/onelogin/features/test/appinfo/domain/AppInfoApi.kt
+++
@@ -1,7 +0,0 @@
-package uk.gov.onelogin.features.test.appinfo.domain
-
-import uk.gov.android.network.api.ApiResponse
-
-fun interface AppInfoApi {
-    suspend fun callApi(): ApiResponse
-}
--- features/src/main/java/uk/gov/onelogin/features/test/appinfo/domain/AppInfoRemoteSource.kt
+++
@@ -1,7 +0,0 @@
-package uk.gov.onelogin.features.test.appinfo.domain
-
-import uk.gov.onelogin.features.appinfo.data.model.AppInfoRemoteState
-
-fun interface AppInfoRemoteSource {
-    suspend fun get(): AppInfoRemoteState
-}
