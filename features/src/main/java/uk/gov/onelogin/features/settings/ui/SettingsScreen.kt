053e345e4d204b9fbc9e7a6e711bcd837e756907
Bianca Mihaila <bianca.mihaila@digital.cabinet-office.gov.uk>
2026-04-27 11:50:57 +0100

    feat: Add modifier that configures different behaviour for tab and arrow up/ down
    Resolves: DCMAW-19031

--- features/src/main/java/uk/gov/onelogin/features/settings/ui/SettingsScreen.kt
+++ features/src/main/java/uk/gov/onelogin/features/settings/ui/SettingsScreen.kt
@@ -6,8 +6,12 @@ import android.content.res.Configuration
 import androidx.activity.compose.BackHandler
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.gestures.animateScrollBy
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -36,9 +40,17 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.input.key.type
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.platform.UriHandler
 import androidx.compose.ui.platform.testTag
@@ -53,6 +65,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kotlinx.coroutines.launch
 import uk.gov.android.onelogin.core.R
 import uk.gov.android.ui.componentsv2.heading.GdsHeading
 import uk.gov.android.ui.componentsv2.heading.GdsHeadingAlignment
@@ -143,12 +156,14 @@ private fun SettingsScreenBody(
     uriHandler: UriHandler,
     settingsScreenLinks: SettingsScreenLinks,
 ) {
+    val scrollState = rememberScrollState()
     Column(
         modifier =
             Modifier
                 .padding(top = paddingValues.calculateTopPadding())
                 .consumeWindowInsets(paddingValues)
-                .verticalScroll(rememberScrollState())
+                .verticalScroll(scrollState)
+                .bringIntoView(scrollState)
                 .windowInsetsPadding(WindowInsets.displayCutout)
                 .padding(top = smallPadding, bottom = xsmallPadding),
     ) {
@@ -597,6 +612,49 @@ internal fun SettingsScreenOptInNoShowBiometricsPreview() {
     }
 }
 
+/**
+ * Adds a downwards and upwards scroll when a keyboard down or up arrow is pressed
+ *
+ * @param scrollState [ScrollState] represents the column scroll state
+ * @return augmented [Modifier]
+ */
+@Composable
+fun Modifier.bringIntoView(scrollState: ScrollState): Modifier {
+    val coroutineScope = rememberCoroutineScope()
+    val interactionSource = remember { MutableInteractionSource() }
+    val focusRequester = remember { FocusRequester() }
+    return this
+        .onKeyEvent {
+            when {
+                it.type == KeyEventType.KeyDown &&
+                    it.key == Key.DirectionDown &&
+                    scrollState.canScrollForward -> {
+                    coroutineScope.launch {
+                        scrollState.animateScrollBy(
+                            SCROLL_MULTIPLIER * scrollState.viewportSize,
+                        )
+                    }
+                    true
+                }
+
+                it.type == KeyEventType.KeyDown &&
+                    it.key == Key.DirectionUp &&
+                    scrollState.canScrollBackward -> {
+                    coroutineScope.launch {
+                        scrollState.animateScrollBy(
+                            -SCROLL_MULTIPLIER * scrollState.viewportSize,
+                        )
+                    }
+                    true
+                }
+
+                else -> false
+            }
+        }.focusRequester(focusRequester)
+        .focusable(interactionSource = interactionSource)
+}
+
+private const val SCROLL_MULTIPLIER = 0.4f
 private const val MANAGE_DETAILS_TRAVERSAL_ORDER = -24f
 private const val HELP_TRAVERSAL_ORDER = -23f
 private const val USING_LINK_TRAVERSAL_ORDER = -22f
