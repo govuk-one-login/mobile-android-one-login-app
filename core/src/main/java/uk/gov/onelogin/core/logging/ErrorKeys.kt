3012f8e0d4b69be87e4130e78a327f6f09934a7d
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2026-05-28 11:08:10 +0100

    feat: Add error keys utility

---
+++ core/src/main/java/uk/gov/onelogin/core/logging/ErrorKeys.kt
@@ -0,0 +1,47 @@
+package uk.gov.onelogin.core.logging
+
+import uk.gov.logging.api.v3.Logger
+import uk.gov.logging.api.v3.customkey.CustomKey
+import uk.gov.onelogin.core.logging.ErrorKeys.actionKey
+import uk.gov.onelogin.core.logging.ErrorKeys.componentKey
+
+object ErrorKeys {
+    private const val COMPONENT = "component"
+    private const val ACTION = "action"
+
+    /**
+     * Creates a [CustomKey] representing the software component where an error occurred.
+     * The value is automatically prefixed with "app.".
+     *
+     * @param component the feature or module, e.g. "login", "wallet.store", "tokens.persistent_id"
+     * @sample errorKeysSample
+     */
+    fun componentKey(component: String): CustomKey {
+        assert(component.matches(Regex("^[a-z][a-z._]*$"))) {
+            "component must be snake case with dot separators only (e.g. 'login' or 'wallet.store'), got: $component"
+        }
+        assert(!component.startsWith("app.")) {
+            "component includes the app prefix, unnecessarily: $component"
+        }
+        return CustomKey.StringKey(ErrorKeys.COMPONENT, "app.$component")
+    }
+
+    /**
+     * Creates a [CustomKey] representing the action being attempted when an error occurred.
+     *
+     * @param action what was being attempted, e.g. "Redirect to app", "Get persistent ID"
+     * @sample errorKeysSample
+     */
+    fun actionKey(action: String): CustomKey = CustomKey.StringKey(ErrorKeys.ACTION, action)
+}
+
+@Suppress("UnusedPrivateMember")
+private fun errorKeysSample(logger: Logger) {
+    logger.error(
+        "LoginUseCase",
+        "Failed to redirect to app",
+        Exception("Failed to redirect to app"),
+        componentKey("login"),
+        actionKey("Redirect to app"),
+    )
+}
