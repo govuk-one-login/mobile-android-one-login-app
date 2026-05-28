7270aa5250161c21119699635f1da17e0bccbe4c
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2026-05-28 11:08:52 +0100

    refactor: Update GetPersistentIdImpl error logging

--- core/src/main/java/uk/gov/onelogin/core/tokens/domain/retrieve/GetPersistentIdImpl.kt
+++ core/src/main/java/uk/gov/onelogin/core/tokens/domain/retrieve/GetPersistentIdImpl.kt
@@ -1,8 +1,10 @@
 package uk.gov.onelogin.core.tokens.domain.retrieve
 
 import uk.gov.android.wallet.sdk.WalletSdk
+import uk.gov.logging.api.LogTagProvider
 import uk.gov.logging.api.v3.Logger
-import uk.gov.logging.api.v3.customkey.CustomKey
+import uk.gov.onelogin.core.logging.ErrorKeys.actionKey
+import uk.gov.onelogin.core.logging.ErrorKeys.componentKey
 import uk.gov.onelogin.core.tokens.utils.AuthTokenStoreKeys
 import javax.inject.Inject
 
@@ -13,37 +15,51 @@ class GetPersistentIdImpl
         val getFromOpenSecureStore: GetFromOpenSecureStore,
         private val walletSdk: WalletSdk,
         private val logger: Logger,
-    ) : GetPersistentId {
+    ) : GetPersistentId,
+        LogTagProvider {
         override suspend fun invoke(): String? {
             val id =
                 getFromOpenSecureStore(AuthTokenStoreKeys.PERSISTENT_ID_KEY)?.get(
                     AuthTokenStoreKeys.PERSISTENT_ID_KEY,
                 )
+
+            // Log an error if the Wallet state is inconsistent with the persistent ID
             try {
                 if (id.isNullOrEmpty() && !walletSdk.isEmpty()) {
-                    val exp = Exception("Wallet is not empty")
-                    val reason = "secure wallet data deleted"
-                    logErrors(exp, reason)
+                    logError(GetPersistentIdException.WalletNotEmpty())
                 }
             } catch (walletError: WalletSdk.WalletSdkError.WalletEmptyCheckFailed) {
-                val reason = walletError.message ?: "could not determine if wallet is empty"
-                logErrors(walletError, reason)
+                logError(GetPersistentIdException.WalletEmptyCheckFailed(walletError))
             } catch (e: Throwable) {
-                val reason = "secure wallet data deleted"
-                logErrors(e, reason)
+                // We don't expect this path to be reachable
+                logError(GetPersistentIdException.WalletEmptyCheckFailed(e))
             }
+
             return id
         }
 
-        private fun logErrors(
-            errorThrown: Throwable,
-            reason: String
-        ) {
+        private fun logError(exception: GetPersistentIdException) =
             logger.error(
-                this.javaClass.simpleName,
-                errorThrown.message ?: "error",
-                errorThrown,
-                CustomKey.StringKey("reason", reason),
+                exception.message,
+                exception,
+                componentKey("tokens.persistent_id"),
+                actionKey("Get persistent ID"),
             )
-        }
     }
+
+internal sealed class GetPersistentIdException(
+    override val message: String,
+    override val cause: Throwable? = null,
+) : RuntimeException() {
+    class WalletEmptyCheckFailed(
+        cause: Throwable
+    ) : GetPersistentIdException(
+            "Wallet SDK failed to check if wallet is empty",
+            cause = cause
+        )
+
+    class WalletNotEmpty :
+        GetPersistentIdException(
+            "No persistent ID but wallet isn't empty"
+        )
+}
