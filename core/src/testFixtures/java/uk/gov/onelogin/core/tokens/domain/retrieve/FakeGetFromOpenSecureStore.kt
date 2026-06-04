d4b656e2dd438b782aa4a70b0d15cfabc6f318e5
Elijah Hezekiah <elijah.hezekiah@digital.cabinet-office.gov.uk>
2026-06-04 15:48:10 +0100

    feat: prompt reauth when walletStoreID is missing from device storage
    - added logger to  GetWalletStoreIdImpl.kt
    - added test for logger on GetWalletStoreIdImplTest.kt
    
    Resolves: DCMAW-20192

--- core/src/testFixtures/java/uk/gov/onelogin/core/tokens/domain/retrieve/FakeGetFromOpenSecureStore.kt
+++ core/src/testFixtures/java/uk/gov/onelogin/core/tokens/domain/retrieve/FakeGetFromOpenSecureStore.kt
@@ -3,5 +3,20 @@ package uk.gov.onelogin.core.tokens.domain.retrieve
 class FakeGetFromOpenSecureStore :
     GetFromOpenSecureStore,
     MutableMap<String, String?> by mutableMapOf() {
-    override suspend fun invoke(vararg key: String): Map<String, String?>? = filterKeys { it in key }.ifEmpty { null }
+    private var exception: Throwable? = null
+    private var returnsNull = false
+
+    fun throwException(e: Throwable) {
+        exception = e
+    }
+
+    fun returnsNull() {
+        returnsNull = true
+    }
+
+    override suspend fun invoke(vararg key: String): Map<String, String?>? {
+        exception?.let { throw it }
+        if (returnsNull) return null
+        return filterKeys { it in key }.ifEmpty { null }
+    }
 }
