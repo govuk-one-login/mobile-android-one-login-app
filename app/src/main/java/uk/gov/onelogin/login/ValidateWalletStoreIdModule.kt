56d3a6aaadd8ac32ca33d0a095f50a803f45d5b1
Elijah Hezekiah <elijah.hezekiah@digital.cabinet-office.gov.uk>
2026-06-16 16:52:51 +0100

    feat: prompt reauth when wallet store id is missing from device storage
    - moved GetWalletModule from ValidateWalletStoreIdModule.kt to TokenModule.kt
    
    Resolves: DCMAW-20192

--- app/src/main/java/uk/gov/onelogin/login/ValidateWalletStoreIdModule.kt
+++ app/src/main/java/uk/gov/onelogin/login/ValidateWalletStoreIdModule.kt
@@ -1,15 +1,12 @@
 package uk.gov.onelogin.login
 
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
-import dagger.hilt.components.SingletonComponent
 import uk.gov.logging.api.v3.Logger
 import uk.gov.onelogin.core.tokens.domain.retrieve.GetPersistentId
 import uk.gov.onelogin.core.tokens.domain.retrieve.GetWalletStoreId
-import uk.gov.onelogin.core.tokens.domain.retrieve.GetWalletStoreIdImpl
 import uk.gov.onelogin.features.login.domain.validateWalletStoreId.ValidateWalletStoreId
 
 @Module
@@ -21,11 +18,4 @@ object ValidateWalletStoreIdModule {
         getPersistentId: GetPersistentId,
         logger: Logger,
     ): ValidateWalletStoreId = ValidateWalletStoreId(getWalletStoreId, getPersistentId, logger)
-}
-
-@Module
-@InstallIn(SingletonComponent::class)
-interface GetWalletModule {
-    @Binds
-    fun bindGetWalletId(getWalletStoreId: GetWalletStoreIdImpl): GetWalletStoreId
 }
