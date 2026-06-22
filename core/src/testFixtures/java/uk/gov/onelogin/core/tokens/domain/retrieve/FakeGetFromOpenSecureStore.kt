b5c87c3cc777d00814ec58a7e56d88bf97d68115
Elijah Hezekiah <elijah.hezekiah@digital.cabinet-office.gov.uk>
2026-06-22 20:15:54 +0100

    feat: Prompt reauth when wallet store id is missing from device storage
    - added  RemoveWalletIdTest.kt
    
    Resolves: DCMAW-20192

--- core/src/testFixtures/java/uk/gov/onelogin/core/tokens/domain/retrieve/FakeGetFromOpenSecureStore.kt
+++ core/src/testFixtures/java/uk/gov/onelogin/core/tokens/domain/retrieve/FakeGetFromOpenSecureStore.kt
@@ -6,10 +6,6 @@ class FakeGetFromOpenSecureStore :
     var exception: Throwable? = null
         private set
 
-    fun throwException(e: Throwable) {
-        exception = e
-    }
-
     override suspend fun invoke(vararg key: String): Map<String, String?>? {
         exception?.let { throw it }
         return filterKeys { it in key }.ifEmpty { null }
