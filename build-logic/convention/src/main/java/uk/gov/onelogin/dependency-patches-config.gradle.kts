179087a77185a89a603546638ef07fe2275fdcc5
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2026-07-15 10:25:27 +0100

    build: Refactor plugin

--- build-logic/convention/src/main/java/uk/gov/onelogin/dependency-patches-config.gradle.kts
+++ build-logic/convention/src/main/java/uk/gov/onelogin/dependency-patches-config.gradle.kts
@@ -5,10 +5,13 @@ import org.gradle.accessors.dm.LibrariesForLibs
 //https://github.com/gradle/gradle/issues/15383
 val libs = the<LibrariesForLibs>()
 
-configurations.all {
-    project.dependencies.constraints.apply {
-        add(name, libs.bouncycastle.bcprov) {
-            because("Earlier versions contain known vulnerabilities")
+dependencies {
+    constraints {
+        configurations.configureEach {
+            val configuration = this@configureEach
+            add(configuration.name, libs.bouncycastle.bcprov) {
+                because("Earlier versions contain known vulnerabilities")
+            }
         }
     }
 }
