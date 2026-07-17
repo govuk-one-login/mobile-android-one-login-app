a8e100b66e4525bbb7e5191bc1beca9b50bf8061
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2026-07-17 11:34:11 +0100

    feat: Add NetworkService

--- app/src/main/java/uk/gov/onelogin/network/NetworkModule.kt
+++ app/src/main/java/uk/gov/onelogin/network/NetworkModule.kt
@@ -3,15 +3,19 @@ package uk.gov.onelogin.network
 import android.content.Context
 import android.net.ConnectivityManager
 import android.os.Build
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import uk.gov.android.network.client.GenericHttpClient
+import uk.gov.android.network.client.GenericHttpClient as GenericHttpClientV1
+import uk.gov.android.network.client.v2.GenericHttpClient as GenericHttpClientV2
 import uk.gov.android.network.client.KtorHttpClient
 import uk.gov.android.network.online.OnlineChecker
 import uk.gov.android.network.online.OnlineCheckerImpl
+import uk.gov.android.network.service.DefaultNetworkService
+import uk.gov.android.network.service.NetworkService
 import uk.gov.android.network.useragent.UserAgent
 import uk.gov.android.network.useragent.UserAgentGenerator
 import uk.gov.android.network.useragent.UserAgentGeneratorImpl
@@ -21,42 +25,71 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object NetworkModule {
-    @Provides
-    fun provideOnlineChecker(
-        @ApplicationContext
-        context: Context,
-    ): OnlineChecker {
-        val connectivityManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        return OnlineCheckerImpl(connectivityManager)
-    }
+interface NetworkModule {
+    @Binds
+    fun networkService(
+        defaultNetworkService: DefaultNetworkService
+    ): NetworkService
 
-    @Provides
-    fun providesUserAgentGenerator(
-        @ApplicationContext context: Context,
-    ): UserAgentGenerator {
-        val userAgentGenerator = UserAgentGeneratorImpl()
-        val appName = context.resources.getString(R.string.one_login_app_name)
-        userAgentGenerator.setUserAgent(
-            UserAgent(
-                appName = appName,
-                versionName = BuildConfig.VERSION_NAME,
-                clientName = BuildConfig.APPLICATION_ID,
-                manufacturer = Build.MANUFACTURER,
-                model = Build.MODEL,
-                sdkVersion = Build.VERSION.SDK_INT,
-                clientVersion = BuildConfig.VERSION_NAME,
-            ),
-        )
-        return userAgentGenerator
-    }
+    @Binds
+    fun genericHttpClientV2(
+        ktorHttpClient: KtorHttpClient
+    ): GenericHttpClientV2
 
-    @Suppress("LongParameterList")
-    @Provides
-    @Singleton
-    fun provideHttpClient(userAgentGenerator: UserAgentGenerator,): GenericHttpClient {
-        val client = KtorHttpClient(userAgentGenerator)
-        return client
+    @Binds
+    fun genericHttpClientV1(
+        ktorHttpClient: KtorHttpClient
+    ): GenericHttpClientV1
+
+    companion object {
+        /**
+         * Provides a single uninitialised instance of [DefaultNetworkService].
+         *
+         * [DefaultNetworkService] is further initialised in [uk.gov.onelogin.MainActivityViewModel]
+         */
+        @Provides
+        @Singleton
+        fun provideNetworkService(
+            httpClient: GenericHttpClientV2
+        ): DefaultNetworkService =
+            DefaultNetworkService(httpClient)
+
+        @Provides
+        fun provideOnlineChecker(
+            @ApplicationContext
+            context: Context,
+        ): OnlineChecker {
+            val connectivityManager =
+                context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            return OnlineCheckerImpl(connectivityManager)
+        }
+
+        @Provides
+        fun providesUserAgentGenerator(
+            @ApplicationContext context: Context,
+        ): UserAgentGenerator {
+            val userAgentGenerator = UserAgentGeneratorImpl()
+            val appName = context.resources.getString(R.string.one_login_app_name)
+            userAgentGenerator.setUserAgent(
+                UserAgent(
+                    appName = appName,
+                    versionName = BuildConfig.VERSION_NAME,
+                    clientName = BuildConfig.APPLICATION_ID,
+                    manufacturer = Build.MANUFACTURER,
+                    model = Build.MODEL,
+                    sdkVersion = Build.VERSION.SDK_INT,
+                    clientVersion = BuildConfig.VERSION_NAME,
+                ),
+            )
+            return userAgentGenerator
+        }
+
+        @Suppress("LongParameterList")
+        @Provides
+        @Singleton
+        fun provideHttpClient(userAgentGenerator: UserAgentGenerator): KtorHttpClient {
+            val client = KtorHttpClient(userAgentGenerator)
+            return client
+        }
     }
 }
--- app/src/test/java/uk/gov/onelogin/MainActivityViewModelTest.kt
+++ app/src/test/java/uk/gov/onelogin/MainActivityViewModelTest.kt
@@ -1,33 +1,71 @@
 package uk.gov.onelogin
 
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
+import uk.gov.android.network.api.v2.ApiRequest
+import uk.gov.android.network.api.v2.ApiResponse
+import uk.gov.android.network.api.v2.ApiResponseAssertions.expectFailure
+import uk.gov.android.network.api.v2.ApiResponseAssertions.expectSuccess
 import uk.gov.android.network.attestation.ClientAttestationProvider
 import uk.gov.android.network.attestation.TestClientAttestationProvider
 import uk.gov.android.network.auth.AuthenticationProvider
 import uk.gov.android.network.auth.TestAuthenticationProvider
 import uk.gov.android.network.client.GenericHttpClient
+import uk.gov.android.network.client.v2.GenericHttpResponse
+import uk.gov.android.network.client.v2.StubHttpClient
 import uk.gov.android.network.dpop.DPoPProvider
 import uk.gov.android.network.dpop.TestDPoPProvider
+import uk.gov.android.network.service.DefaultNetworkService
 
 class MainActivityViewModelTest {
     private val genericHttpClient: GenericHttpClient = mock()
+    private val okResponse = GenericHttpResponse(200, "ok")
+    private val stubHttpClient = StubHttpClient().apply {
+        response = okResponse
+    }
+    private val defaultNetworkService = DefaultNetworkService(stubHttpClient)
     private val authenticationProvider: AuthenticationProvider = TestAuthenticationProvider()
     private var dPoPProvider: DPoPProvider = TestDPoPProvider()
     private var clientAttestationProvider: ClientAttestationProvider = TestClientAttestationProvider()
     private val vm =
         MainActivityViewModel(
             genericHttpClient,
+            defaultNetworkService,
             authenticationProvider,
             dPoPProvider,
             clientAttestationProvider,
         )
 
     @Test
-    fun `setHttpClientAuthProvider sets the authentication provider on the http client`() {
-        vm.setHttpClientAuthProvider()
+    fun `initialiseNetworkService sets the authentication provider on the http client`() {
+        vm.initialiseNetworkService()
 
         verify(genericHttpClient).setAuthenticationProvider(authenticationProvider)
     }
+
+    @Test
+    fun `initialiseNetworkService configures the network service`() = runTest {
+        // Before initialising the network service, it won't work
+        defaultNetworkService.makeRequest().expectFailure()
+
+        vm.initialiseNetworkService()
+
+        val success = defaultNetworkService.makeRequest().expectSuccess()
+        assertEquals(
+            ApiResponse.Success(okResponse.body, okResponse.status),
+            success
+        )
+    }
+
+    private suspend fun DefaultNetworkService.makeRequest() =
+        makeRequest(
+            ApiRequest.Get("https://example.gov.uk")
+        ) {
+            withAttestation = true
+            withAuthentication("scope")
+            withRefreshDPoP = true
+        }
 }
