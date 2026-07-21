3635e76f9a257d5200495589be3c36f63d9bc846
jonnyandrew <4940864+jonnyandrew@users.noreply.github.com>
2026-07-21 11:51:12 +0100

    feat: DCMAW-20189 Add NetworkService to dependency graph (#951)

--- app/src/main/java/uk/gov/onelogin/network/NetworkModule.kt
+++ app/src/main/java/uk/gov/onelogin/network/NetworkModule.kt
@@ -3,6 +3,7 @@ package uk.gov.onelogin.network
 import android.content.Context
 import android.net.ConnectivityManager
 import android.os.Build
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,6 +13,8 @@ import uk.gov.android.network.client.GenericHttpClient
 import uk.gov.android.network.client.KtorHttpClient
 import uk.gov.android.network.online.OnlineChecker
 import uk.gov.android.network.online.OnlineCheckerImpl
+import uk.gov.android.network.service.DefaultNetworkService
+import uk.gov.android.network.service.NetworkService
 import uk.gov.android.network.useragent.UserAgent
 import uk.gov.android.network.useragent.UserAgentGenerator
 import uk.gov.android.network.useragent.UserAgentGeneratorImpl
@@ -21,42 +24,68 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object NetworkModule {
-    @Provides
-    fun provideOnlineChecker(
-        @ApplicationContext
-        context: Context,
-    ): OnlineChecker {
-        val connectivityManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        return OnlineCheckerImpl(connectivityManager)
-    }
+interface NetworkModule {
+    @Binds
+    fun networkService(
+        defaultNetworkService: DefaultNetworkService
+    ): NetworkService
 
-    @Provides
-    fun providesUserAgentGenerator(
-        @ApplicationContext context: Context,
-    ): UserAgentGenerator {
-        val userAgentGenerator = UserAgentGeneratorImpl()
-        val appName = context.resources.getString(R.string.one_login_app_name)
-        userAgentGenerator.setUserAgent(
-            UserAgent(
-                appName = appName,
-                versionName = BuildConfig.VERSION_NAME,
-                clientName = BuildConfig.APPLICATION_ID,
-                manufacturer = Build.MANUFACTURER,
-                model = Build.MODEL,
-                sdkVersion = Build.VERSION.SDK_INT,
-                clientVersion = BuildConfig.VERSION_NAME,
-            ),
-        )
-        return userAgentGenerator
-    }
+    /**
+     * [GenericHttpClient] will be deprecated. Prefer [NetworkService] for API requests.
+     */
+    @Binds
+    fun genericHttpClient(
+        ktorHttpClient: KtorHttpClient
+    ): GenericHttpClient
 
-    @Suppress("LongParameterList")
-    @Provides
-    @Singleton
-    fun provideHttpClient(userAgentGenerator: UserAgentGenerator,): GenericHttpClient {
-        val client = KtorHttpClient(userAgentGenerator)
-        return client
+    companion object {
+        /**
+         * Provides a single uninitialised instance of [DefaultNetworkService].
+         *
+         * [DefaultNetworkService] is further initialised in [uk.gov.onelogin.MainActivityViewModel]
+         */
+        @Provides
+        @Singleton
+        fun provideNetworkService(
+            ktorHttpClient: KtorHttpClient
+        ): DefaultNetworkService =
+            DefaultNetworkService(ktorHttpClient)
+
+        @Provides
+        fun provideOnlineChecker(
+            @ApplicationContext
+            context: Context,
+        ): OnlineChecker {
+            val connectivityManager =
+                context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            return OnlineCheckerImpl(connectivityManager)
+        }
+
+        @Provides
+        fun providesUserAgentGenerator(
+            @ApplicationContext context: Context,
+        ): UserAgentGenerator {
+            val userAgentGenerator = UserAgentGeneratorImpl()
+            val appName = context.resources.getString(R.string.one_login_app_name)
+            userAgentGenerator.setUserAgent(
+                UserAgent(
+                    appName = appName,
+                    versionName = BuildConfig.VERSION_NAME,
+                    clientName = BuildConfig.APPLICATION_ID,
+                    manufacturer = Build.MANUFACTURER,
+                    model = Build.MODEL,
+                    sdkVersion = Build.VERSION.SDK_INT,
+                    clientVersion = BuildConfig.VERSION_NAME,
+                ),
+            )
+            return userAgentGenerator
+        }
+
+        @Provides
+        @Singleton
+        fun provideHttpClient(userAgentGenerator: UserAgentGenerator): KtorHttpClient {
+            val client = KtorHttpClient(userAgentGenerator)
+            return client
+        }
     }
 }
