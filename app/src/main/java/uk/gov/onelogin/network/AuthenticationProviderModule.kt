ffb84f08e17e067935e848aa76e40e5f79215527
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2026-07-23 16:46:49 +0100

    refactor: Upgrade StsAuthenticationProvider to use NetworkService V2

--- app/src/main/java/uk/gov/onelogin/network/AuthenticationProviderModule.kt
+++ app/src/main/java/uk/gov/onelogin/network/AuthenticationProviderModule.kt
@@ -1,6 +1,7 @@
 package uk.gov.onelogin.network
 
 import android.content.Context
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -11,6 +12,7 @@ import uk.gov.android.network.client.GenericHttpClient
 import uk.gov.android.onelogin.core.R
 import uk.gov.logging.api.v3.Logger
 import uk.gov.onelogin.core.navigation.domain.Navigator
+import uk.gov.onelogin.core.network.di.StsUrl
 import uk.gov.onelogin.core.tokens.data.TokenRepository
 import uk.gov.onelogin.core.tokens.domain.expirychecks.IsTokenExpired
 import uk.gov.onelogin.core.utils.AccessToken
@@ -21,35 +23,21 @@ import uk.gov.onelogin.features.signout.domain.SignOutUseCase
 
 @Module
 @InstallIn(ViewModelComponent::class)
-object AuthenticationProviderModule {
-    @Suppress("LongParameterList")
-    @Provides
-    fun provideAuthenticationProvider(
-        activityProvider: ActivityProvider,
-        @ApplicationContext context: Context,
-        genericHttpClient: GenericHttpClient,
-        tokenRepository: TokenRepository,
-        @AccessToken isAccessTokenExpired: IsTokenExpired,
-        navigator: Navigator,
-        refreshExchange: RefreshExchange,
-        signOutUseCase: SignOutUseCase,
-        logger: Logger,
-    ): AuthenticationProvider {
-        val stsUrl =
+interface AuthenticationProviderModule {
+    @Binds
+    fun authenticationProvider(
+        stsAuthenticationProvider: StsAuthenticationProvider,
+    ): AuthenticationProvider
+
+    companion object {
+        @Provides
+        @StsUrl
+        fun provideStsUrl(
+            @ApplicationContext context: Context,
+        ): String =
             context.getString(
                 R.string.stsUrl,
                 context.getString(R.string.tokenExchangeEndpoint),
             )
-        return StsAuthenticationProvider(
-            activityProvider,
-            stsUrl,
-            tokenRepository,
-            isAccessTokenExpired,
-            genericHttpClient,
-            navigator,
-            refreshExchange,
-            signOutUseCase,
-            logger,
-        )
     }
 }
