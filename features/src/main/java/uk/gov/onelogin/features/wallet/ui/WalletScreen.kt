704d49cecd1a35fb024719fa7dd1b14ea8d28113
jonnyandrew <4940864+jonnyandrew@users.noreply.github.com>
2026-08-19 10:28:22 +0100

    fix: DCMAW-22138 Workaround for crash occurring when wallet store ID initialisation is cancelled (#1007)
    Fixes a crash when attempting to add a wallet credential from a deep link from a cold start (DCMAW-22138).
    
    The root cause, that Secure Store consumes coroutine cancellations and doesn't propagate them to calling code (DCMAW-22143), can be fixed separately.

--- features/src/main/java/uk/gov/onelogin/features/wallet/ui/WalletScreen.kt
+++ features/src/main/java/uk/gov/onelogin/features/wallet/ui/WalletScreen.kt
@@ -3,22 +3,24 @@ package uk.gov.onelogin.features.wallet.ui
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.key
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import uk.gov.onelogin.core.ui.pages.loading.LoadingScreen
+import uk.gov.onelogin.core.ui.pages.loading.LoadingScreenAnalyticsViewModel
 
 @Composable
 fun WalletScreen(
     deepLinkRoute: Boolean,
     setDisplayContentAsFullScreen: (Boolean) -> Unit,
     viewModel: WalletScreenViewModel = hiltViewModel(),
+    loadingAnalyticsViewModel: LoadingScreenAnalyticsViewModel = hiltViewModel(),
 ) {
 
 
     key(deepLinkRoute) {
         when (val state = viewModel.state.collectAsState().value) {
             is WalletScreenState.Loading -> {
-                LoadingScreen { }
+                LoadingScreen(analyticsViewModel = loadingAnalyticsViewModel) { }
             }
             is WalletScreenState.Display ->
                 state.walletAppDisplayer.WalletApp()
--- features/src/test/java/uk/gov/onelogin/features/unit/wallet/WalletScreenViewModelTest.kt
+++ features/src/test/java/uk/gov/onelogin/features/unit/wallet/WalletScreenViewModelTest.kt
@@ -1,16 +1,22 @@
 package uk.gov.onelogin.features.unit.wallet
 
 import app.cash.turbine.test
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.yield
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.RegisterExtension
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import uk.gov.android.wallet.sdk.WalletSdk
+import uk.gov.logging.api.v3.Logger
 import uk.gov.onelogin.core.tokens.domain.retrieve.GetWalletStoreId
 import uk.gov.onelogin.core.ui.wallet.WalletAppDisplayerImpl
 import uk.gov.onelogin.features.extensions.CoroutinesTestExtension
@@ -98,6 +104,48 @@ class WalletScreenViewModelTest {
             "Wallet Store ID must not be null. This is guaranteed by ValidateWalletStoreId",
             cause.message,
         )
+    }
+
+    @Test
+    fun `given wallet store ID is null but coroutines are cancelled before retrieval, it does not crash`() {
+        val getWalletStoreIdCancelling = GetWalletStoreId {
+            currentCoroutineContext().cancel()
+            yield()
+            null
+        }
+        assertDoesNotThrow {
+            runTest {
+                WalletScreenViewModel(
+                    walletSdk = walletSdk,
+                    getWalletStoreId = getWalletStoreIdCancelling,
+                    walletAppDisplayer = walletAppDisplayer,
+                )
+            }
+        }
+    }
+
+    // This test is redundant once DCMAW-22143 is fixed.
+    @Test
+    fun `given wallet store ID is null because coroutine cancellation isn't propogated, it does not crash`() {
+        val getWalletStoreIdCancelling = GetWalletStoreId {
+            try {
+                currentCoroutineContext().cancel()
+                yield()
+            } catch (_: CancellationException) {
+                // Simulate the incorrect behaviour of Secure Store
+                // by consuming cancellation exceptions (DCMAW-22143).
+            }
+            null
+        }
+        assertDoesNotThrow {
+            runTest {
+                WalletScreenViewModel(
+                    walletSdk = walletSdk,
+                    getWalletStoreId = getWalletStoreIdCancelling,
+                    walletAppDisplayer = walletAppDisplayer,
+                )
+            }
+        }
     }
 
     companion object {
