ebdb124e840e08a1e1d20da91d90e358d6513735
jonnyandrew <4940864+jonnyandrew@users.noreply.github.com>
2026-08-19 16:43:39 +0100

    fix: DCMAW-22137 Handle requests from Wallet SDK to enter full screen (#1008)
    Fixes an issue where Wallet SDK couldn't enter full screen.
    
    This was happening because we were using two separate instances of `WalletSdk` to 1) display the `WalletApp()` composable and 2) listen for full screen requests.
    
    To fix this, we listen for full screen requests from the same instance of `WalletSdk` that displays `WalletApp()`.
    
    Another solution would have been to scope `WalletSdk` as a `@Singleton`, but I decided against this because we've discussed potentially migrating it away from `@SingletonScope` in future. Making it a `@Singleton` now could make that change harder over time.

--- features/src/main/java/uk/gov/onelogin/features/wallet/ui/WalletScreen.kt
+++ features/src/main/java/uk/gov/onelogin/features/wallet/ui/WalletScreen.kt
@@ -2,9 +2,9 @@ package uk.gov.onelogin.features.wallet.ui
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import uk.gov.onelogin.core.ui.pages.loading.LoadingScreen
 import uk.gov.onelogin.core.ui.pages.loading.LoadingScreenAnalyticsViewModel
 
@@ -16,20 +16,18 @@ fun WalletScreen(
     loadingAnalyticsViewModel: LoadingScreenAnalyticsViewModel = hiltViewModel(),
 ) {
 
+    val state: WalletScreenState by viewModel.state.collectAsState()
 
     key(deepLinkRoute) {
-        when (val state = viewModel.state.collectAsState().value) {
+        when (val state = state) {
             is WalletScreenState.Loading -> {
                 LoadingScreen(analyticsViewModel = loadingAnalyticsViewModel) { }
             }
-            is WalletScreenState.Display ->
-                state.walletAppDisplayer.WalletApp()
+            is WalletScreenState.Display -> {
+                state.walletDisplayer.Wallet(
+                    onFullScreenRequest = setDisplayContentAsFullScreen
+                )
+            }
         }
     }
-
-    setDisplayContentAsFullScreen(
-        viewModel.walletSdk.displayAsFullScreen
-            .collectAsStateWithLifecycle()
-            .value,
-    )
 }
--- features/src/main/java/uk/gov/onelogin/features/wallet/ui/WalletScreenState.kt
+++ features/src/main/java/uk/gov/onelogin/features/wallet/ui/WalletScreenState.kt
@@ -1,13 +1,13 @@
 package uk.gov.onelogin.features.wallet.ui
 
-import uk.gov.onelogin.core.ui.wallet.WalletAppDisplayer
+import uk.gov.onelogin.core.ui.wallet.WalletDisplayer
 
 /**
  * The sealed class is  the UI state of [WalletScreen].
  *
  *
  * - [Loading] is the initial state. It acts as a structural guard to ensure the Wallet SDK
- *   is never rendered before [WalletAppDisplayer]
+ *   is never rendered before [WalletDisplayer]
  *
  * - [Display] is set after the wallet store ID has been retrieved and supplied to the SDK.
  *   The screen remains in this state for all subsequent navigations.
@@ -18,6 +18,6 @@ sealed interface WalletScreenState {
     data object Loading : WalletScreenState
 
     data class Display(
-        val walletAppDisplayer: WalletAppDisplayer,
+        val walletDisplayer: WalletDisplayer,
     ) : WalletScreenState
 }
