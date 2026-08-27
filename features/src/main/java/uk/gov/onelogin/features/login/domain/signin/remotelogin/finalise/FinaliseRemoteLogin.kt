182c7624a5fa3c0ac4b463e809bc8fc87e7cb170
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2026-08-27 11:42:44 +0100

    docs: Fix comment

--- features/src/main/java/uk/gov/onelogin/features/login/domain/signin/remotelogin/finalise/FinaliseRemoteLogin.kt
+++ features/src/main/java/uk/gov/onelogin/features/login/domain/signin/remotelogin/finalise/FinaliseRemoteLogin.kt
@@ -2,11 +2,17 @@ package uk.gov.onelogin.features.login.domain.signin.remotelogin.finalise
 
 import android.content.Intent
 import uk.gov.android.authentication.login.TokenResponse
+import uk.gov.onelogin.features.login.domain.signin.remotelogin.start.StartRemoteLogin
 
 /**
- * Use case allowing to perform login (first time user) and
+ * Gets access and refresh tokens by exchanging the response from the browser login redirect.
+ *
+ * The caller is responsible for saving the tokens.
  */
 fun interface FinaliseRemoteLogin {
+    /**
+     * @param intent The intent received after starting a remote login with [StartRemoteLogin]
+     */
     suspend fun handle(
         intent: Intent,
         onFailure: (Throwable?) -> Unit,
