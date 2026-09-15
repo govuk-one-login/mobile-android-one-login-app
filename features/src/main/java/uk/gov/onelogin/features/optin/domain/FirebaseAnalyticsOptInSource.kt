84840baf4caf810010f64fb866a3ed298d400372
jonnyandrew <4940864+jonnyandrew@users.noreply.github.com>
2026-09-15 16:20:25 +0100

    refactor: Add AnalyticsLogger V3 to dependency graph (#1049)

--- features/src/main/java/uk/gov/onelogin/features/optin/domain/FirebaseAnalyticsOptInSource.kt
+++ features/src/main/java/uk/gov/onelogin/features/optin/domain/FirebaseAnalyticsOptInSource.kt
@@ -3,7 +3,8 @@ package uk.gov.onelogin.features.optin.domain
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import uk.gov.logging.api.analytics.logging.AnalyticsLogger
+import uk.gov.logging.api.analytics.logging.AnalyticsLogger as AnalyticsLoggerV1
+import uk.gov.logging.api.analytics.logging.v3.AnalyticsLogger
 import uk.gov.onelogin.features.optin.data.AnalyticsOptInState
 import uk.gov.onelogin.features.optin.domain.source.OptInRemoteSource
 import uk.gov.onelogin.features.optin.ui.IODispatcherQualifier
@@ -12,6 +13,7 @@ import javax.inject.Inject
 class FirebaseAnalyticsOptInSource
     @Inject
     constructor(
+        private val analyticsV1: AnalyticsLoggerV1,
         private val analytics: AnalyticsLogger,
         @param:IODispatcherQualifier
         private val dispatcher: CoroutineDispatcher = Dispatchers.IO,
@@ -19,9 +21,15 @@ class FirebaseAnalyticsOptInSource
         override suspend fun update(state: AnalyticsOptInState) {
             withContext(dispatcher) {
                 when (state) {
-                    AnalyticsOptInState.No, AnalyticsOptInState.None -> analytics.setEnabled(false)
+                    AnalyticsOptInState.No, AnalyticsOptInState.None -> {
+                        analytics.setEnabled(false)
+                        analyticsV1.setEnabled(false)
+                    }
 
-                    AnalyticsOptInState.Yes -> analytics.setEnabled(true)
+                    AnalyticsOptInState.Yes -> {
+                        analytics.setEnabled(true)
+                        analyticsV1.setEnabled(true)
+                    }
                 }
             }
         }
