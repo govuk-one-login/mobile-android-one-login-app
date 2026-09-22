f9551b2a060b9bbf571b8deea031f11eb0b60481
Jonny Andrew <4940864+jonnyandrew@users.noreply.github.com>
2026-09-22 15:33:20 +0100

    test: Enable screenshots in core module

--- features/build.gradle.kts
+++ features/build.gradle.kts
@@ -6,12 +6,10 @@ plugins {
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.oss.licence.about.libraries)
-    alias(libs.plugins.paparazzi)
+    id("uk.gov.onelogin.screenshot-test-config")
     alias(libs.plugins.ksp)
     id("uk.gov.onelogin.plugin.test-config")
 }
-
-apply(from = rootProject.file("gradle/snapshot-test-filter.gradle.kts"))
 
 testTypeConfig {
     testTypes(
